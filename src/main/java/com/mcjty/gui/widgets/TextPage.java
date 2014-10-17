1c9fb1dd26dea9359f7df7ec32bcff4000b1113d
McJty <mcjty1@gmail.com>
2014-10-17 10:41:19 +0200

    Hyperlinks work in the in-game book

--- src/main/java/com/mcjty/gui/widgets/TextPage.java
+++ src/main/java/com/mcjty/gui/widgets/TextPage.java
@@ -3,53 +3,260 @@ package com.mcjty.gui.widgets;
 import com.mcjty.gui.Window;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.resources.IResource;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.input.Keyboard;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TextPage extends AbstractWidget<TextPage> {
-    private final List<String> lines = new ArrayList<String>();
+    private final List<Page> pages = new ArrayList<Page>();
+    private final Map<String,Integer> nodes = new HashMap<String, Integer>();
+
+    private int pageIndex = 0;
+    private final List<Line> lines = new ArrayList<Line>();
+    private final List<Link> links = new ArrayList<Link>();
 
     public TextPage(Minecraft mc, Gui gui) {
         super(mc, gui);
     }
 
-    public TextPage addLine(String line) {
-        lines.add(line);
-        return this;
-    }
-
-    public TextPage setPage(Page page) {
+    private void setPage(Page page) {
         lines.clear();
-        for (String line : page.lines) {
-            addLine(line);
+        links.clear();
+        if (!pages.isEmpty()) {
+            int y = 3;
+            for (Line line : page.lines) {
+                lines.add(line);
+                if (line.isLink()) {
+                    links.add(new Link(y, y+13, line.node));
+                }
+                y += 14;
+            }
         }
+    }
+
+    private void newPage(TextPage.Page page) {
+        if (!page.isEmpty()) {
+            pages.add(page);
+        }
+    }
+
+    public int getPageIndex() {
+        return pageIndex;
+    }
+
+    public int getPageCount() {
+        return pages.size();
+    }
+
+    public TextPage setText(ResourceLocation manualResource) {
+        TextPage.Page page = new TextPage.Page();
+        try {
+            IResourceManager resourceManager = mc.getResourceManager();
+            IResource iresource = resourceManager.getResource(manualResource);
+            InputStream inputstream = iresource.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputstream, "UTF-8"));
+            String line = br.readLine();
+            while (line != null) {
+                if ("{}".equals(line)) {
+                    newPage(page);
+                    page = new TextPage.Page();
+                } else {
+                    Line l = page.addLine(line);
+                    if (l.isNode()) {
+                        nodes.put(l.node, pages.size());
+                    }
+                }
+                line = br.readLine();
+            }
+            newPage(page);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        showCurrentPage();
         return this;
     }
 
+    public void prevPage() {
+        pageIndex--;
+        if (pageIndex < 0) {
+            pageIndex = 0;
+        }
+        showCurrentPage();
+    }
+
+    public void nextPage() {
+        pageIndex++;
+        if (pageIndex >= pages.size()) {
+            pageIndex = pages.size()-1;
+        }
+        showCurrentPage();
+    }
+
+    private void showCurrentPage() {
+        setPage(pages.get(pageIndex));
+    }
+
+    @Override
+    public Widget mouseClick(Window window, int x, int y, int button) {
+        if (enabled) {
+            window.setTextFocus(this);
+            for (Link link : links) {
+                if (link.y1 <= y && y <= link.y2) {
+                    Integer page = nodes.get(link.node);
+                    if (page != null) {
+                        pageIndex = page;
+                        showCurrentPage();
+                        return this;
+                    }
+                }
+            }
+            return this;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean keyTyped(Window window, char typedChar, int keyCode) {
+        boolean rc = super.keyTyped(window, typedChar, keyCode);
+        if (rc) {
+            return true;
+        }
+        if (enabled) {
+            if (keyCode == Keyboard.KEY_BACK || keyCode == Keyboard.KEY_LEFT) {
+                prevPage();
+                return true;
+            } else if (keyCode == Keyboard.KEY_SPACE || keyCode == Keyboard.KEY_RIGHT) {
+                nextPage();
+                return true;
+            } else if (keyCode == Keyboard.KEY_HOME) {
+                pageIndex = 0;
+                showCurrentPage();
+            } else if (keyCode == Keyboard.KEY_END) {
+                if (!pages.isEmpty()) {
+                    pageIndex = pages.size()-1;
+                    showCurrentPage();
+                }
+            }
+        }
+        return false;
+    }
 
     @Override
     public void draw(Window window, int x, int y) {
         super.draw(window, x, y);
 
         y += 3;
-        for (String line : lines) {
-            mc.fontRenderer.drawString(mc.fontRenderer.trimStringToWidth(line, bounds.width), x + bounds.x, y + bounds.y, 0xFF000000);
-            y += 14;
+        int dx;
+        for (Line line : lines) {
+            System.out.println("line.line = " + line.line);
+            if (line.line != null) {
+                String s = "";
+                int col = 0xFF000000;
+                dx = 0;
+                if (line.isBold()) {
+                    char c = 167;
+                    s = Character.toString(c) + "l";
+                }
+                if (line.isLink()) {
+                    char c = 167;
+                    s = Character.toString(c) + "n";
+                    col = 0xFF0040AA;
+                    dx = 25;
+                }
+                s += line.line;
+                mc.fontRenderer.drawString(mc.fontRenderer.trimStringToWidth(s, bounds.width-dx), x + dx + bounds.x, y + bounds.y, col);
+                y += 14;
+            }
+        }
+    }
+
+    private static class Line {
+        private boolean bold;
+        private boolean islink;
+        private boolean isnode;
+        String node;
+        String line;
+
+        public boolean isBold() {
+            return bold;
+        }
+
+        public boolean isLink() {
+            return islink;
+        }
+
+        public boolean isNode() {
+            return isnode;
+        }
+
+        Line(String line) {
+            bold = false;
+            islink = false;
+            node = null;
+
+            if (line.startsWith("{b}")) {
+                bold = true;
+                this.line = line.substring(3);
+            } else if (line.startsWith("{n:")) {
+                int end = line.indexOf('}');
+                if (end == -1) {
+                    // Error, just put in the entire line
+                    this.line = line;
+                } else {
+                    node = line.substring(3, end);
+                    isnode = true;
+                    this.line = null;
+                }
+            } else if (line.startsWith("{l:")) {
+                int end = line.indexOf('}');
+                if (end == -1) {
+                    // Error, just put in the entire line
+                    this.line = line;
+                } else {
+                    node = line.substring(3, end);
+                    islink = true;
+                    this.line = line.substring(end+1);
+                }
+            } else {
+                this.line = line;
+            }
         }
     }
 
     public static class Page {
-        final List<String> lines = new ArrayList<String>();
+        final List<Line> lines = new ArrayList<Line>();
 
         public boolean isEmpty() {
             return lines.isEmpty();
         }
 
-        public void addLine(String line) {
-            lines.add(line);
+        public Line addLine(String line) {
+            Line l = new Line(line);
+            lines.add(l);
+            return l;
         }
     }
 
+    public static class Link {
+        int y1;
+        int y2;
+        String node;
+
+        public Link(int y1, int y2, String node) {
+            this.y1 = y1;
+            this.y2 = y2;
+            this.node = node;
+        }
+    }
 
 }
--- src/main/java/com/mcjty/rftools/items/manual/GuiRFToolsManual.java
+++ src/main/java/com/mcjty/rftools/items/manual/GuiRFToolsManual.java
@@ -4,22 +4,16 @@ import com.mcjty.gui.Window;
 import com.mcjty.gui.events.ButtonEvent;
 import com.mcjty.gui.layout.HorizontalLayout;
 import com.mcjty.gui.layout.VerticalLayout;
+import com.mcjty.gui.widgets.*;
 import com.mcjty.gui.widgets.Button;
+import com.mcjty.gui.widgets.Label;
 import com.mcjty.gui.widgets.Panel;
-import com.mcjty.gui.widgets.TextPage;
-import com.mcjty.gui.widgets.Widget;
 import com.mcjty.rftools.RFTools;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.resources.IResource;
-import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.input.Mouse;
-import org.lwjgl.input.Keyboard;
 
 import java.awt.*;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GuiRFToolsManual extends GuiScreen {
 
@@ -28,101 +22,46 @@ public class GuiRFToolsManual extends GuiScreen {
     /** The Y size of the window in pixels. */
     protected int ySize = 180;
 
-    private int pageIndex = 0;
     private Window window;
     private TextPage textPage;
+    private Label pageLabel;
     private Button prevButton;
     private Button nextButton;
 
     private static final ResourceLocation manualText = new ResourceLocation(RFTools.MODID, "docs/manual.txt");
 
-    private final List<TextPage.Page> pages = new ArrayList<TextPage.Page>();
-
     public GuiRFToolsManual() {
-    }
-
-    private void newPage(TextPage.Page page) {
-        if (!page.isEmpty()) {
-            pages.add(page);
-        }
-    }
-
-    static String convertStreamToString(InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
-
-    private void readManual() {
-        TextPage.Page page = new TextPage.Page();
-        try {
-            IResourceManager resourceManager = mc.getResourceManager();
-            IResource iresource = resourceManager.getResource(manualText);
-            InputStream inputstream = iresource.getInputStream();
-            String manualText = convertStreamToString(inputstream);
-            String[] lines = manualText.split("\n");
-
-            for (String line : lines) {
-                if ("---".equals(line)) {
-                    newPage(page);
-                    page = new TextPage.Page();
-                } else {
-                    page.addLine(line);
-                }
-            }
-            newPage(page);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void initGui() {
         super.initGui();
 
-        readManual();
-
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
 
-        textPage = new TextPage(mc, this).setPage(pages.get(pageIndex));
+        textPage = new TextPage(mc, this).setText(manualText);
 
         prevButton = new Button(mc, this).setText("<").addButtonEvent(new ButtonEvent() {
             @Override
             public void buttonClicked(Widget parent) {
-                prevPage();
+                textPage.prevPage();
             }
         });
+        pageLabel = new Label(mc, this).setText("0 / 0");
         nextButton = new Button(mc, this).setText(">").addButtonEvent(new ButtonEvent() {
             @Override
             public void buttonClicked(Widget parent) {
-                nextPage();
+                textPage.nextPage();
             }
         });
-        Panel buttonPanel = new Panel(mc, this).setLayout(new HorizontalLayout()).setDesiredHeight(16).addChild(prevButton).addChild(nextButton);
+        Panel buttonPanel = new Panel(mc, this).setLayout(new HorizontalLayout()).setDesiredHeight(16).addChild(prevButton).addChild(pageLabel).addChild(nextButton);
 
         Widget toplevel = new Panel(mc, this).setFilledRectThickness(2).setLayout(new VerticalLayout()).addChild(textPage).addChild(buttonPanel);
         toplevel.setBounds(new Rectangle(k, l, xSize, ySize));
 
         window = new Window(this, toplevel);
-    }
-
-    private void prevPage() {
-        pageIndex--;
-        if (pageIndex < 0) {
-            pageIndex = 0;
-        }
-        textPage.setPage(pages.get(pageIndex));
-    }
-
-    private void nextPage() {
-        pageIndex++;
-        if (pageIndex >= pages.size()) {
-            pageIndex = pages.size()-1;
-        }
-        textPage.setPage(pages.get(pageIndex));
+        window.setTextFocus(textPage);
     }
 
     @Override
@@ -152,21 +91,18 @@ public class GuiRFToolsManual extends GuiScreen {
     @Override
     protected void keyTyped(char typedChar, int keyCode) {
         super.keyTyped(typedChar, keyCode);
-        if (!window.keyTyped(typedChar, keyCode)) {
-            if (keyCode == Keyboard.KEY_BACK || keyCode == Keyboard.KEY_LEFT) {
-                prevPage();
-            } else if (keyCode == Keyboard.KEY_SPACE || keyCode == Keyboard.KEY_RIGHT) {
-                nextPage();
-            }
-        }
+        window.keyTyped(typedChar, keyCode);
     }
 
     @Override
     public void drawScreen(int xSize_lo, int ySize_lo, float par3) {
         super.drawScreen(xSize_lo, ySize_lo, par3);
 
-        prevButton.setEnabled(pageIndex > 0);
-        nextButton.setEnabled(pageIndex < pages.size()-1);
+        int index = textPage.getPageIndex();
+        int count = textPage.getPageCount();
+        pageLabel.setText((index + 1) + "/" + count);
+        prevButton.setEnabled(index > 0);
+        nextButton.setEnabled(index < count - 1);
 
         window.draw();
         java.util.List<String> tooltips = window.getTooltips();
