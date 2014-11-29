5130df7f563547eb586057146cc7bc978b5760b3
McJty <mcjty1@gmail.com>
2014-11-29 18:59:30 +0100

    Increased the radius of the damage check for the shield system so that nearby entities get damage sooner.

--- src/main/java/com/mcjty/rftools/blocks/shield/ShieldBlockTileEntity.java
+++ src/main/java/com/mcjty/rftools/blocks/shield/ShieldBlockTileEntity.java
@@ -81,7 +81,7 @@ public class ShieldBlockTileEntity extends TileEntity {
         }
         damageTimer = 10;
         if (beamBox == null) {
-            beamBox = AxisAlignedBB.getBoundingBox(xCoord - .2, yCoord - .2, zCoord - .2, xCoord + 1.2, yCoord + 1.2, zCoord + 1.2);
+            beamBox = AxisAlignedBB.getBoundingBox(xCoord - .4, yCoord - .4, zCoord - .4, xCoord + 1.4, yCoord + 1.4, zCoord + 1.4);
         }
 
         if (shieldBlock != null) {
