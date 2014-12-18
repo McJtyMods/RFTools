bae5530b2b3933eded3044c179d15ce8600b0384
McJty <mcjty1@gmail.com>
2014-12-18 18:47:46 +0100

    When we're using the integrated server we don't sync dimension info to the client as I suspect this causes sudden inventory loss from time to time.

--- src/main/java/com/mcjty/rftools/PlayerLoginEvent.java
+++ src/main/java/com/mcjty/rftools/PlayerLoginEvent.java
@@ -2,17 +2,14 @@ package com.mcjty.rftools;
 
 import com.mcjty.rftools.dimension.RfToolsDimensionManager;
 import com.mcjty.rftools.network.DimensionSyncPacket;
-import com.mcjty.rftools.network.PacketHandler;
-import com.mcjty.rftools.network.PacketRegisterDimensions;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent;
 import cpw.mods.fml.common.network.FMLOutboundHandler;
 import cpw.mods.fml.common.network.handshake.NetworkDispatcher;
 import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.server.MinecraftServer;
 
 public class PlayerLoginEvent {
 //    @SubscribeEvent
@@ -28,20 +25,22 @@ public class PlayerLoginEvent {
 
     @SubscribeEvent
     public void onConnectionCreated(FMLNetworkEvent.ServerConnectionFromClientEvent event) {
-        RFTools.log("onConnectionCreated");
 
-        DimensionSyncPacket packet = new DimensionSyncPacket();
+        if (MinecraftServer.getServer().isDedicatedServer()) {
+            RFTools.log("SMP: Sync dimensions to client");
+            DimensionSyncPacket packet = new DimensionSyncPacket();
 
-        EntityPlayer player = ((NetHandlerPlayServer) event.handler).playerEntity;
-        RfToolsDimensionManager manager = RfToolsDimensionManager.getDimensionManager(player.getEntityWorld());
-        for (Integer id : manager.getDimensions().keySet()) {
-            RFTools.log("Sending over dimension " + id + " to the client");
-            packet.addDimension(id);
+            EntityPlayer player = ((NetHandlerPlayServer) event.handler).playerEntity;
+            RfToolsDimensionManager manager = RfToolsDimensionManager.getDimensionManager(player.getEntityWorld());
+            for (Integer id : manager.getDimensions().keySet()) {
+                RFTools.log("Sending over dimension " + id + " to the client");
+                packet.addDimension(id);
+            }
+
+            RFTools.channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.DISPATCHER);
+            RFTools.channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(event.manager.channel().attr(NetworkDispatcher.FML_DISPATCHER).get());
+            RFTools.channels.get(Side.SERVER).writeOutbound(packet);
         }
-
-        RFTools.channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.DISPATCHER);
-        RFTools.channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(event.manager.channel().attr(NetworkDispatcher.FML_DISPATCHER).get());
-        RFTools.channels.get(Side.SERVER).writeOutbound(packet);
     }
 
 
