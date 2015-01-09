015988ec7358661e309040aa1da2db6a88e16e4c
McJty <mcjty1@gmail.com>
2015-01-09 05:40:16 +0100

    Fixed a bug with handling of empty containers. This could cause crashes in some situations with the GUI's of the relay, monitor, endergenic, ...

--- src/main/java/com/mcjty/container/EmptyContainer.java
+++ src/main/java/com/mcjty/container/EmptyContainer.java
@@ -1,6 +1,9 @@
 package com.mcjty.container;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 /**
  * Use this in case you want a container with no slots (for example, for energy storage only).
@@ -9,5 +12,9 @@ public class EmptyContainer extends GenericContainer {
 
     public EmptyContainer(EntityPlayer player) {
         super(EmptyContainerFactory.getInstance(), player);
+    }
+
+    @Override
+    public void putStackInSlot(int index, ItemStack stack) {
     }
 }
