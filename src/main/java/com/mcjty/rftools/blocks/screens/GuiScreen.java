17cd794f9a9935ffc408afde39c2718796c68504
McJty <mcjty1@gmail.com>
2015-01-24 05:28:30 +0100

    Cleanup

--- src/main/java/com/mcjty/rftools/blocks/screens/GuiScreen.java
+++ src/main/java/com/mcjty/rftools/blocks/screens/GuiScreen.java
@@ -43,7 +43,7 @@ public class GuiScreen  extends GenericGuiContainer<SimpleScreenTileEntity> {
         toplevel = new Panel(mc, this).setBackground(iconLocation).setLayout(new PositionalLayout());
 
         for (int i = 0 ; i < 7 ; i++) {
-            buttons[i] = new ToggleButton(mc, this).setLayoutHint(new PositionalLayout.PositionalHint(30, 7 + i*18 + 1, 55, 16)).setEnabled(false);
+            buttons[i] = new ToggleButton(mc, this).setLayoutHint(new PositionalLayout.PositionalHint(30, 7 + i * 18 + 1, 55, 16)).setEnabled(false);
             final int finalI = i;
             buttons[i].addButtonEvent(new ButtonEvent() {
                 @Override
@@ -75,54 +75,62 @@ public class GuiScreen  extends GenericGuiContainer<SimpleScreenTileEntity> {
         for (int i = 0 ; i < 7 ; i++) {
             final ItemStack slot = tileEntity.getStackInSlot(i);
             if (slot != null && slot.getItem() != null && slot.getItem() instanceof ModuleProvider) {
-                buttons[i].setEnabled(true);
                 ModuleProvider moduleProvider = (ModuleProvider) slot.getItem();
                 Class<? extends ClientScreenModule> clientScreenModuleClass = moduleProvider.getClientScreenModule();
                 if (!clientScreenModuleClass.isInstance(clientScreenModules[i])) {
-                    toplevel.removeChild(modulePanels[i]);
-                    try {
-                        ClientScreenModule clientScreenModule = clientScreenModuleClass.newInstance();
-                        clientScreenModules[i] = clientScreenModule;
-                    } catch (InstantiationException e) {
-                        throw new RuntimeException(e);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    NBTTagCompound tagCompound = slot.getTagCompound();
-                    if (tagCompound == null) {
-                        tagCompound = new NBTTagCompound();
-                    }
-
-                    final NBTTagCompound finalTagCompound = tagCompound;
-                    final int finalI = i;
-                    modulePanels[i] = clientScreenModules[i].createGui(mc, this, tagCompound, new ModuleGuiChanged() {
-                        @Override
-                        public void updateData() {
-                            slot.setTagCompound(finalTagCompound);
-                            tileEntity.setInventorySlotContents(finalI, slot);
-                        }
-                    });
-                    modulePanels[i].setLayoutHint(new PositionalLayout.PositionalHint(90, 7, 140, 130));
-                    toplevel.addChild(modulePanels[i]);
+                    installModuleGui(i, slot, moduleProvider, clientScreenModuleClass);
                 }
-
-                buttons[i].setText(moduleProvider.getName());
             } else {
-                buttons[i].setEnabled(false);
-                buttons[i].setText("");
-                clientScreenModules[i] = null;
-                toplevel.removeChild(modulePanels[i]);
-                modulePanels[i] = null;
-                if (selected == i) {
-                    selected = -1;
-                }
+                uninstallModuleGui(i);
             }
             if (modulePanels[i] != null) {
                 modulePanels[i].setVisible(selected == i);
                 buttons[i].setPressed(selected == i);
             }
         }
+    }
+
+    private void uninstallModuleGui(int i) {
+        buttons[i].setEnabled(false);
+        buttons[i].setPressed(false);
+        buttons[i].setText("");
+        clientScreenModules[i] = null;
+        toplevel.removeChild(modulePanels[i]);
+        modulePanels[i] = null;
+        if (selected == i) {
+            selected = -1;
+        }
+    }
+
+    private void installModuleGui(int i, final ItemStack slot, ModuleProvider moduleProvider, Class<? extends ClientScreenModule> clientScreenModuleClass) {
+        buttons[i].setEnabled(true);
+        toplevel.removeChild(modulePanels[i]);
+        try {
+            ClientScreenModule clientScreenModule = clientScreenModuleClass.newInstance();
+            clientScreenModules[i] = clientScreenModule;
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        NBTTagCompound tagCompound = slot.getTagCompound();
+        if (tagCompound == null) {
+            tagCompound = new NBTTagCompound();
+        }
+
+        final NBTTagCompound finalTagCompound = tagCompound;
+        final int finalI = i;
+        modulePanels[i] = clientScreenModules[i].createGui(mc, this, tagCompound, new ModuleGuiChanged() {
+            @Override
+            public void updateData() {
+                slot.setTagCompound(finalTagCompound);
+                tileEntity.setInventorySlotContents(finalI, slot);
+            }
+        });
+        modulePanels[i].setLayoutHint(new PositionalLayout.PositionalHint(90, 7, 140, 130));
+        toplevel.addChild(modulePanels[i]);
+        buttons[i].setText(moduleProvider.getName());
     }
 
     @Override
