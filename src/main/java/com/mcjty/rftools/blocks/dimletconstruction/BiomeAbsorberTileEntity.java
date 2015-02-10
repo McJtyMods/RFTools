a5a44d4e3e985f901261925ec15b3b880237e728
McJty <mcjty1@gmail.com>
2015-02-10 09:35:16 +0100

    Better biome absorber textures and biome absorber no longer 'sparkles' when it is full.

--- src/main/java/com/mcjty/rftools/blocks/dimletconstruction/BiomeAbsorberTileEntity.java
+++ src/main/java/com/mcjty/rftools/blocks/dimletconstruction/BiomeAbsorberTileEntity.java
@@ -14,17 +14,18 @@ public class BiomeAbsorberTileEntity extends GenericTileEntity {
 
     @Override
     protected void checkStateClient() {
-        Random rand = worldObj.rand;
+        if (absorbing > 0) {
+            Random rand = worldObj.rand;
 
-        double u = rand.nextFloat() * 2.0f - 1.0f;
-        double v = (float) (rand.nextFloat() * 2.0f * Math.PI);
-        double x = Math.sqrt(1 - u * u) * Math.cos(v);
-        double y = Math.sqrt(1 - u * u) * Math.sin(v);
-        double z = u;
-        double r = 1.0f;
+            double u = rand.nextFloat() * 2.0f - 1.0f;
+            double v = (float) (rand.nextFloat() * 2.0f * Math.PI);
+            double x = Math.sqrt(1 - u * u) * Math.cos(v);
+            double y = Math.sqrt(1 - u * u) * Math.sin(v);
+            double z = u;
+            double r = 1.0f;
 
-        worldObj.spawnParticle("portal", xCoord + 0.5f + x * r, yCoord + 0.5f + y * r, zCoord + 0.5f + z * r, -x, -y, -z);
-//        ((WorldServer)worldObj).func_147487_a("portal", xCoord + 0.5f + x * r, yCoord + 0.5f + y * r, zCoord + 0.5f + z * r, 2, -x, -y, -z, 0.1f);
+            worldObj.spawnParticle("portal", xCoord + 0.5f + x * r, yCoord + 0.5f + y * r, zCoord + 0.5f + z * r, -x, -y, -z);
+        }
     }
 
     @Override
