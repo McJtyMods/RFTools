385edbe49c96e0a185214f99f56db7a6a4331330
McJty <mcjty1@gmail.com>
2015-02-14 05:44:24 +0100

    Version 2.41

--- src/main/java/com/mcjty/rftools/RFTools.java
+++ src/main/java/com/mcjty/rftools/RFTools.java
@@ -38,7 +38,7 @@ import java.util.EnumMap;
 @Mod(modid = RFTools.MODID, version = RFTools.VERSION)
 public class RFTools {
     public static final String MODID = "rftools";
-    public static final String VERSION = "2.40";
+    public static final String VERSION = "2.41";
 
     @SidedProxy(clientSide="com.mcjty.rftools.ClientProxy", serverSide="com.mcjty.rftools.ServerProxy")
     public static CommonProxy proxy;
