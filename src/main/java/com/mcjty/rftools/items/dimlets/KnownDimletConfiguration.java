40f780d436c6fd15ba9f727c900f2c62b263a717
McJty <mcjty1@gmail.com>
2015-03-02 14:08:43 +0100

    Added a very rare wither dimlet.

--- src/main/java/com/mcjty/rftools/items/dimlets/KnownDimletConfiguration.java
+++ src/main/java/com/mcjty/rftools/items/dimlets/KnownDimletConfiguration.java
@@ -16,6 +16,7 @@ import com.mcjty.varia.BlockMeta;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.entity.monster.*;
 import net.minecraft.entity.passive.*;
 import net.minecraft.init.Blocks;
@@ -325,6 +326,7 @@ public class KnownDimletConfiguration {
         initMobItem(cfg, mainCfg, EntitySquid.class, "Squid", mapping, 10, 3, 4, 40);
         initMobItem(cfg, mainCfg, EntityWolf.class, "Wolf", mapping, 10, 3, 4, 20);
         initMobItem(cfg, mainCfg, EntityVillager.class, "Villager", mapping, 10, 3, 4, 20);
+        initMobItem(cfg, mainCfg, EntityWither.class, "Wither", mapping, 5, 1, 2, 5);
         addExtraInformation(idDefaultMobs, "With this default dimlet you will just get", "the default mob spawning");
 
         int idSkyNormal = initSkyItem(cfg, mainCfg, "Normal", new SkyDescriptor.Builder().skyType(SkyType.SKY_NORMAL).build(), false, mapping);
