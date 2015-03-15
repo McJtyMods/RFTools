474ccb5a7bdb7b8abcc659b0e9faebbc3df73d13
McJty <mcjty1@gmail.com>
2015-03-15 08:27:57 +0100

    Implemented dimlet validation in the dimension enscriber.

--- src/main/java/com/mcjty/rftools/blocks/dimlets/GuiDimensionEnscriber.java
+++ src/main/java/com/mcjty/rftools/blocks/dimlets/GuiDimensionEnscriber.java
@@ -10,14 +10,25 @@ import com.mcjty.gui.widgets.Panel;
 import com.mcjty.gui.widgets.TextField;
 import com.mcjty.gui.widgets.Widget;
 import com.mcjty.rftools.RFTools;
+import com.mcjty.rftools.dimension.world.types.ControllerType;
+import com.mcjty.rftools.dimension.world.types.FeatureType;
+import com.mcjty.rftools.dimension.world.types.TerrainType;
 import com.mcjty.rftools.items.ModItems;
+import com.mcjty.rftools.items.dimlets.DimletKey;
+import com.mcjty.rftools.items.dimlets.DimletObjectMapping;
+import com.mcjty.rftools.items.dimlets.DimletType;
+import com.mcjty.rftools.items.dimlets.KnownDimletConfiguration;
 import com.mcjty.rftools.network.Argument;
+import com.mcjty.varia.Counter;
+import net.minecraft.client.Minecraft;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 public class GuiDimensionEnscriber extends GenericGuiContainer<DimensionEnscriberTileEntity> {
     public static final int ENSCRIBER_WIDTH = 256;
@@ -26,6 +37,7 @@ public class GuiDimensionEnscriber extends GenericGuiContainer<DimensionEnscribe
     private Button extractButton;
     private Button storeButton;
     private TextField nameField;
+    private TextField validateField;
 
     private static final ResourceLocation iconLocation = new ResourceLocation(RFTools.MODID, "textures/gui/dimensionenscriber.png");
 
@@ -62,10 +74,12 @@ public class GuiDimensionEnscriber extends GenericGuiContainer<DimensionEnscribe
                 storeName(newText);
             }
         }).setLayoutHint(new PositionalLayout.PositionalHint(13, 200, 60, 16));
+        validateField = new TextField(mc, this).setText("Val").setLayoutHint(new PositionalLayout.PositionalHint(35, 142, 38, 16)).
+                setTooltips("Hover here for errors...");
         setNameFromDimensionTab();
 
         Widget toplevel = new Panel(mc, this).setBackground(iconLocation).setLayout(new PositionalLayout()).addChild(extractButton).addChild(storeButton).
-                addChild(nameField);
+                addChild(nameField).addChild(validateField);
         toplevel.setBounds(new Rectangle(guiLeft, guiTop, xSize, ySize));
 
         window = new Window(this, toplevel);
@@ -104,11 +118,125 @@ public class GuiDimensionEnscriber extends GenericGuiContainer<DimensionEnscribe
         }
     }
 
+    private List<DimletKey> extractModifiersForType(List<DimletKey> modifiers, DimletType type) {
+        List<DimletKey> modifiersForType = new ArrayList<DimletKey>();
+        int i = 0;
+        while (i < modifiers.size()) {
+            DimletKey modifier = modifiers.get(i);
+            if (type.isModifiedBy(modifier.getType())) {
+                modifiersForType.add(modifier);
+                modifiers.remove(i);
+            } else {
+                i++;
+            }
+        }
+        return modifiersForType;
+    }
 
+    private String shortenName(String name) {
+        int idx = name.indexOf('_');
+        if (idx == -1) {
+            return name;
+        } else {
+            return name.substring(idx+1);
+        }
+    }
+
+    private void validateDimlets() {
+        List<String> tooltips = new ArrayList<String>();
+
+        TerrainType terrainType = null;
+        int cntTerrain = 0;
+        int cntBiomes = 0;
+        int cntController = 0;
+        for (int i = DimensionEnscriberContainer.SLOT_DIMLETS ; i < DimensionEnscriberContainer.SLOT_TAB ; i++) {
+            Slot slot = inventorySlots.getSlot(i);
+            if (slot != null && slot.getStack() != null && slot.getStack().stackSize > 0) {
+                ItemStack stack = slot.getStack();
+                DimletKey key = KnownDimletConfiguration.getDimletKey(stack, Minecraft.getMinecraft().theWorld);
+                if (key.getType() == DimletType.DIMLET_TERRAIN) {
+                    cntTerrain++;
+                    terrainType = DimletObjectMapping.idToTerrainType.get(key);
+                } else if (key.getType() == DimletType.DIMLET_BIOME) {
+                    cntBiomes++;
+                } else if (key.getType() == DimletType.DIMLET_CONTROLLER) {
+                    cntController++;
+                }
+            }
+        }
+        if (cntTerrain > 1) {
+            tooltips.add("Using more then one TERRAIN is not useful!");
+            terrainType = null;
+        }
+        if (cntController > 1) {
+            tooltips.add("Using more then one CONTROLLER is not useful!");
+        }
+
+        List<DimletKey> modifiers = new ArrayList<DimletKey>();
+        for (int i = DimensionEnscriberContainer.SLOT_DIMLETS ; i < DimensionEnscriberContainer.SLOT_TAB ; i++) {
+            Slot slot = inventorySlots.getSlot(i);
+            if (slot != null && slot.getStack() != null && slot.getStack().stackSize > 0) {
+                ItemStack stack = slot.getStack();
+                DimletKey key = KnownDimletConfiguration.getDimletKey(stack, Minecraft.getMinecraft().theWorld);
+                DimletType type = key.getType();
+                if (type.isModifier()) {
+                    modifiers.add(key);
+                } else {
+                    List<DimletKey> modifiersForType = extractModifiersForType(modifiers, type);
+                    if (type == DimletType.DIMLET_TERRAIN) {
+                        if (DimletObjectMapping.idToTerrainType.get(key) == TerrainType.TERRAIN_VOID) {
+                            tooltips.add("VOID terrain cannot use modifiers");
+                        }
+                    } else if (type == DimletType.DIMLET_FEATURE) {
+                        FeatureType featureType = DimletObjectMapping.idToFeatureType.get(key);
+                        Counter<DimletType> modifierAmountUsed = new Counter<DimletType>();
+                        for (DimletKey modifier : modifiersForType) {
+                            modifierAmountUsed.increment(modifier.getType());
+                        }
+                        for (Map.Entry<DimletType, Integer> entry : modifierAmountUsed.entrySet()) {
+                            Integer amountSupported = featureType.getSupportedModifierAmount(entry.getKey());
+                            if (amountSupported == null) {
+                                tooltips.add(shortenName(featureType.name()) + " does not use " + shortenName(entry.getKey().name()) + " modifiers!");
+                            } else if (amountSupported == 1 && entry.getValue() > 1) {
+                                tooltips.add(shortenName(featureType.name()) + " only needs one " + shortenName(entry.getKey().name()) + " modifier!");
+                            }
+                        }
+
+                        if (terrainType == null && !featureType.supportsAllTerrains()) {
+                            tooltips.add(shortenName(featureType.name()) + " does not work on all terrains and no terrain was specified!");
+                        }
+                        if (terrainType != null && !featureType.isTerrainSupported(terrainType)) {
+                            tooltips.add(shortenName(featureType.name()) + " does not work for terrain " + shortenName(terrainType.name()) + "!");
+                        }
+                    } else if (type == DimletType.DIMLET_CONTROLLER) {
+                        ControllerType controllerType = DimletObjectMapping.idToControllerType.get(key);
+                        int neededBiomes = controllerType.getNeededBiomes();
+                        if (neededBiomes != -1) {
+                            if (cntBiomes > neededBiomes) {
+                                tooltips.add("Too many biomes specified for " + shortenName(controllerType.name()) + "!");
+                            } else if (cntBiomes < neededBiomes) {
+                                tooltips.add("Too few biomes specified for " + shortenName(controllerType.name()) + "!");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!modifiers.isEmpty()) {
+            tooltips.add("There are dangling modifiers in this descriptor");
+        }
+
+        if (tooltips.isEmpty()) {
+            tooltips.add("Everything appears to be allright");
+        }
+        validateField.setTooltips(tooltips.toArray(new String[tooltips.size()]));
+    }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float v, int i, int i2) {
         enableButtons();
+        validateDimlets();
 
         if (tileEntity.hasTabSlotChangedAndClear()) {
             setNameFromDimensionTab();
--- src/main/java/com/mcjty/rftools/dimension/world/types/FeatureType.java
+++ src/main/java/com/mcjty/rftools/dimension/world/types/FeatureType.java
@@ -1,36 +1,69 @@
 package com.mcjty.rftools.dimension.world.types;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import com.mcjty.rftools.items.dimlets.DimletType;
+
+import java.util.*;
 
 import static com.mcjty.rftools.dimension.world.types.TerrainType.*;
 
 public enum FeatureType {
-    FEATURE_NONE,
-    FEATURE_CAVES(TERRAIN_AMPLIFIED, TERRAIN_CAVERN, TERRAIN_CAVERN_OLD, TERRAIN_FLOODED_CAVERN, TERRAIN_CHAOTIC, TERRAIN_FLAT, TERRAIN_ISLAND, TERRAIN_ISLANDS, TERRAIN_LOW_CAVERN, TERRAIN_NORMAL, TERRAIN_PLATEAUS),
-    FEATURE_RAVINES(TERRAIN_AMPLIFIED, TERRAIN_CAVERN, TERRAIN_CAVERN_OLD, TERRAIN_FLOODED_CAVERN, TERRAIN_CHAOTIC, TERRAIN_FLAT, TERRAIN_ISLAND, TERRAIN_ISLANDS, TERRAIN_LOW_CAVERN, TERRAIN_NORMAL, TERRAIN_PLATEAUS),
-    FEATURE_ORBS,
-    FEATURE_OREGEN,
-    FEATURE_LAKES(TERRAIN_AMPLIFIED, TERRAIN_CAVERN, TERRAIN_CAVERN_OLD, TERRAIN_CHAOTIC, TERRAIN_FLAT, TERRAIN_ISLAND, TERRAIN_ISLANDS, TERRAIN_LOW_CAVERN, TERRAIN_NORMAL, TERRAIN_PLATEAUS),
-    FEATURE_TENDRILS,
-    FEATURE_CANYONS,
-    FEATURE_MAZE(TERRAIN_AMPLIFIED, TERRAIN_FLAT, TERRAIN_NORMAL),
-    FEATURE_LIQUIDORBS,
-    FEATURE_SHALLOW_OCEAN(TERRAIN_PLATEAUS, TERRAIN_ISLANDS, TERRAIN_ISLAND, TERRAIN_CHAOTIC),
-    FEATURE_VOLCANOES;
+    FEATURE_NONE(null, null),
+    FEATURE_CAVES(new TSet(TERRAIN_AMPLIFIED, TERRAIN_CAVERN, TERRAIN_CAVERN_OLD, TERRAIN_FLOODED_CAVERN, TERRAIN_CHAOTIC, TERRAIN_FLAT, TERRAIN_ISLAND, TERRAIN_ISLANDS, TERRAIN_LOW_CAVERN, TERRAIN_NORMAL, TERRAIN_PLATEAUS), null),
+    FEATURE_RAVINES(new TSet(TERRAIN_AMPLIFIED, TERRAIN_CAVERN, TERRAIN_CAVERN_OLD, TERRAIN_FLOODED_CAVERN, TERRAIN_CHAOTIC, TERRAIN_FLAT, TERRAIN_ISLAND, TERRAIN_ISLANDS, TERRAIN_LOW_CAVERN, TERRAIN_NORMAL, TERRAIN_PLATEAUS), null),
+    FEATURE_ORBS(null, new MMap(1, DimletType.DIMLET_MATERIAL)),
+    FEATURE_OREGEN(null, new MMap(-1, DimletType.DIMLET_MATERIAL)),
+    FEATURE_LAKES(new TSet(TERRAIN_AMPLIFIED, TERRAIN_CAVERN, TERRAIN_CAVERN_OLD, TERRAIN_CHAOTIC, TERRAIN_FLAT, TERRAIN_ISLAND, TERRAIN_ISLANDS, TERRAIN_LOW_CAVERN, TERRAIN_NORMAL, TERRAIN_PLATEAUS), new MMap(-1, DimletType.DIMLET_LIQUID)),
+    FEATURE_TENDRILS(null, new MMap(1, DimletType.DIMLET_MATERIAL)),
+    FEATURE_CANYONS(null, new MMap(1, DimletType.DIMLET_MATERIAL)),
+    FEATURE_MAZE(new TSet(TERRAIN_AMPLIFIED, TERRAIN_FLAT, TERRAIN_NORMAL), null),
+    FEATURE_LIQUIDORBS(null, new MMap(1, DimletType.DIMLET_MATERIAL, DimletType.DIMLET_LIQUID)),
+    FEATURE_SHALLOW_OCEAN(new TSet(TERRAIN_PLATEAUS, TERRAIN_ISLANDS, TERRAIN_ISLAND, TERRAIN_CHAOTIC), new MMap(1, DimletType.DIMLET_LIQUID)),
+    FEATURE_VOLCANOES(null, null);
 
     private final Set<TerrainType> supportedTerrains;
+    private final Map<DimletType,Integer> supportedModifiers;
 
     /**
      * If no terrain types are given then they are all supported.
      */
-    FeatureType(TerrainType... terrainTypes) {
-        supportedTerrains = new HashSet<TerrainType>();
-        Collections.addAll(supportedTerrains, terrainTypes);
+    FeatureType(Set<TerrainType> terrainTypes, Map<DimletType,Integer> modifiers) {
+        if (terrainTypes == null) {
+            supportedTerrains = Collections.emptySet();
+        } else {
+            supportedTerrains = new HashSet<TerrainType>(terrainTypes);
+        }
+        if (modifiers == null) {
+            supportedModifiers = Collections.emptyMap();
+        } else {
+            supportedModifiers = new HashMap<DimletType, Integer>(modifiers);
+        }
     }
 
     public boolean isTerrainSupported(TerrainType type) {
         return supportedTerrains.isEmpty() || supportedTerrains.contains(type);
+    }
+
+    public boolean supportsAllTerrains() {
+        return supportedTerrains.isEmpty();
+    }
+
+    public Integer getSupportedModifierAmount(DimletType type) {
+        return supportedModifiers.get(type);
+    }
+
+    private static class TSet extends HashSet<TerrainType> {
+        private TSet(TerrainType... terrainTypes) {
+            for (TerrainType type : terrainTypes) {
+                add(type);
+            }
+        }
+    }
+
+    private static class MMap extends HashMap<DimletType,Integer> {
+        private MMap(int amount, DimletType... dimletTypes) {
+            for (DimletType type : dimletTypes) {
+                put(type, amount);
+            }
+        }
     }
 }
