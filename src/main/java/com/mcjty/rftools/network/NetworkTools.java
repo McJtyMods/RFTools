c36a838c4744b8f5cc5a8a961166dc47b332a443
McJty <mcjty1@gmail.com>
2015-04-01 09:28:22 +0200

    Correctly transfer the amount of items in a big itemstack (for DSU and Barrels) from server to client so that the item monitor can display the amount correctly.

--- src/main/java/com/mcjty/rftools/network/NetworkTools.java
+++ src/main/java/com/mcjty/rftools/network/NetworkTools.java
@@ -8,23 +8,28 @@ import net.minecraft.network.PacketBuffer;
 import java.io.IOException;
 
 public class NetworkTools {
+    /// This function supports itemstacks with more then 64 items.
     public static ItemStack readItemStack(ByteBuf dataIn) {
         PacketBuffer buf = new PacketBuffer(dataIn);
         try {
             NBTTagCompound nbt = buf.readNBTTagCompoundFromBuffer();
-            return ItemStack.loadItemStackFromNBT(nbt);
+            ItemStack stack = ItemStack.loadItemStackFromNBT(nbt);
+            stack.stackSize = buf.readInt();
+            return stack;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
+    /// This function supports itemstacks with more then 64 items.
     public static void writeItemStack(ByteBuf dataOut, ItemStack itemStack) {
         PacketBuffer buf = new PacketBuffer(dataOut);
         NBTTagCompound nbt = new NBTTagCompound();
         itemStack.writeToNBT(nbt);
         try {
             buf.writeNBTTagCompoundToBuffer(nbt);
+            buf.writeInt(itemStack.stackSize);
         } catch (IOException e) {
             e.printStackTrace();
         }
