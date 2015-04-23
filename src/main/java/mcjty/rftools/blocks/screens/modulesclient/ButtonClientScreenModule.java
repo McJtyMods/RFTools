c5fd66c0354b6c64ea198c16538ff0632cd7311f
McJty <mcjty1@gmail.com>
2015-04-23 06:37:33 +0200

    Started work on 'toggle' button functionality. Doesn't work yet.

--- src/main/java/mcjty/rftools/blocks/screens/modulesclient/ButtonClientScreenModule.java
+++ src/main/java/mcjty/rftools/blocks/screens/modulesclient/ButtonClientScreenModule.java
@@ -1,6 +1,7 @@
 package mcjty.rftools.blocks.screens.modulesclient;
 
 import mcjty.gui.RenderHelper;
+import mcjty.gui.events.ButtonEvent;
 import mcjty.gui.events.ColorChoiceEvent;
 import mcjty.gui.events.TextEvent;
 import mcjty.gui.layout.HorizontalLayout;
@@ -18,6 +19,7 @@ import org.lwjgl.opengl.GL11;
 public class ButtonClientScreenModule implements ClientScreenModule {
     private String line = "";
     private String button = "";
+    private boolean toggle = false;
     private int color = 0xffffff;
     private int buttonColor = 0xffffff;
     private boolean activated = false;
@@ -96,6 +98,15 @@ public class ButtonClientScreenModule implements ClientScreenModule {
             }
         });
 
+        final ToggleButton toggleButton = new ToggleButton(mc, gui).setText("Toggle").setTooltips("Toggle button mode").setDesiredHeight(13).setCheckMarker(true);
+        toggleButton.addButtonEvent(new ButtonEvent() {
+            @Override
+            public void buttonClicked(Widget parent) {
+                currentData.setBoolean("toggle", toggleButton.isPressed());
+                moduleGuiChanged.updateData();
+            }
+        });
+        panel.addChild(toggleButton);
 
         if (currentData != null) {
             textField.setText(currentData.getString("text"));
@@ -108,6 +119,7 @@ public class ButtonClientScreenModule implements ClientScreenModule {
             if (currentButtonColor != 0) {
                 buttonColorSelector.setCurrentColor(currentButtonColor);
             }
+            toggleButton.setPressed(currentData.getBoolean("toggle"));
         }
 
         panel.addChild(new Panel(mc, gui).setLayout(new HorizontalLayout()).
@@ -136,6 +148,7 @@ public class ButtonClientScreenModule implements ClientScreenModule {
             } else {
                 buttonColor = 0xffffff;
             }
+            toggle = tagCompound.getBoolean("toggle");
         }
     }
 
