fd39d47ff6db571714ac1ecea77f5cce8b3599ae
McJty <mcjty1@gmail.com>
2015-04-29 05:49:36 +0200

    Proper names for block protector and smart wrench.

--- src/main/java/mcjty/rftools/blocks/blockprotector/BlockProtectorBlock.java
+++ src/main/java/mcjty/rftools/blocks/blockprotector/BlockProtectorBlock.java
@@ -2,12 +2,16 @@ package mcjty.rftools.blocks.blockprotector;
 
 import mcjty.api.Infusable;
 import mcjty.container.GenericContainerBlock;
+import mcjty.rftools.RFTools;
 import net.minecraft.block.material.Material;
 
 public class BlockProtectorBlock extends GenericContainerBlock implements Infusable {
 
     public BlockProtectorBlock() {
         super(Material.iron, BlockProtectorTileEntity.class);
+        setBlockName("blockProtectorBlock");
+        setHorizRotation(true);
+        setCreativeTab(RFTools.tabRfTools);
     }
 
     @Override
