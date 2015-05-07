46863a6d7e784201a60c51d983f1a318cbf20dd2
McJty <mcjty1@gmail.com>
2015-05-07 13:58:47 +0200

    Enabled the WIP modular storage block again

--- src/main/java/mcjty/rftools/blocks/storage/ModularStorageSetup.java
+++ src/main/java/mcjty/rftools/blocks/storage/ModularStorageSetup.java
@@ -15,25 +15,25 @@ public class ModularStorageSetup {
     public static ModularStorageBlock modularStorageBlock;
 
     public static void setupBlocks() {
-//        modularStorageBlock = new ModularStorageBlock();
-//        GameRegistry.registerBlock(modularStorageBlock, GenericItemBlock.class, "modularStorageBlock");
-//        GameRegistry.registerTileEntity(ModularStorageTileEntity.class, "ModularStorageTileEntity");
+        modularStorageBlock = new ModularStorageBlock();
+        GameRegistry.registerBlock(modularStorageBlock, GenericItemBlock.class, "modularStorageBlock");
+        GameRegistry.registerTileEntity(ModularStorageTileEntity.class, "ModularStorageTileEntity");
     }
 
     public static void setupCrafting() {
-//        GameRegistry.addRecipe(new ItemStack(modularStorageBlock), "rcr", "qMq", "rqr", 'M', ModBlocks.machineFrame, 'c', Blocks.chest, 'r', Items.redstone, 'q', Items.quartz);
-//
-//        GameRegistry.addRecipe(new ItemStack(ModItems.storageModuleItem, 1, 0), " c ", "gig", " r ", 'p', Items.paper, 'r', Items.redstone, 'i', Items.iron_ingot,
-//                'g', Items.gold_nugget, 'c', Blocks.chest);
-//        GameRegistry.addRecipe(new PreservingShapedRecipe(3, 3, new ItemStack[] {
-//                null, new ItemStack(Blocks.chest), null,
-//                new ItemStack(Items.gold_ingot), new ItemStack(ModItems.storageModuleItem, 1, 0), new ItemStack(Items.gold_ingot),
-//                null, new ItemStack(Items.redstone), null},
-//                new ItemStack(ModItems.storageModuleItem, 1, 1), 4));
-//        GameRegistry.addRecipe(new PreservingShapedRecipe(3, 3, new ItemStack[] {
-//                null, new ItemStack(Blocks.chest), null,
-//                new ItemStack(Blocks.gold_block), new ItemStack(ModItems.storageModuleItem, 1, 1), new ItemStack(Blocks.gold_block),
-//                null, new ItemStack(Items.redstone), null},
-//                new ItemStack(ModItems.storageModuleItem, 1, 2), 4));
+        GameRegistry.addRecipe(new ItemStack(modularStorageBlock), "rcr", "qMq", "rqr", 'M', ModBlocks.machineFrame, 'c', Blocks.chest, 'r', Items.redstone, 'q', Items.quartz);
+
+        GameRegistry.addRecipe(new ItemStack(ModItems.storageModuleItem, 1, 0), " c ", "gig", " r ", 'p', Items.paper, 'r', Items.redstone, 'i', Items.iron_ingot,
+                'g', Items.gold_nugget, 'c', Blocks.chest);
+        GameRegistry.addRecipe(new PreservingShapedRecipe(3, 3, new ItemStack[] {
+                null, new ItemStack(Blocks.chest), null,
+                new ItemStack(Items.gold_ingot), new ItemStack(ModItems.storageModuleItem, 1, 0), new ItemStack(Items.gold_ingot),
+                null, new ItemStack(Items.redstone), null},
+                new ItemStack(ModItems.storageModuleItem, 1, 1), 4));
+        GameRegistry.addRecipe(new PreservingShapedRecipe(3, 3, new ItemStack[] {
+                null, new ItemStack(Blocks.chest), null,
+                new ItemStack(Blocks.gold_block), new ItemStack(ModItems.storageModuleItem, 1, 1), new ItemStack(Blocks.gold_block),
+                null, new ItemStack(Items.redstone), null},
+                new ItemStack(ModItems.storageModuleItem, 1, 2), 4));
     }
 }
