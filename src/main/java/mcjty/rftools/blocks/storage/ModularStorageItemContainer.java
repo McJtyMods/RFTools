4941ea108a9f0b8ed782af24ca44a4ce5c2a2877
McJty <mcjty1@gmail.com>
2015-06-12 19:53:06 +0200

    Fixed a problem where picking up the tablet (or shift-clicking it) while that tablet inventory was open would crash the game (and get rid of tablet + contents). Partially fixed it for shift-click and simply drag. However, using the numeric key of that specific slot (1 to 9) still causes this corruption so don't do that!

--- src/main/java/mcjty/rftools/blocks/storage/ModularStorageItemContainer.java
+++ src/main/java/mcjty/rftools/blocks/storage/ModularStorageItemContainer.java
@@ -10,6 +10,7 @@ public class ModularStorageItemContainer extends GenericContainer {
     public static final String CONTAINER_INVENTORY = "container";
 
     private EntityPlayer entityPlayer;
+    private int tabletIndex;
 
     public static final ContainerFactory factory = new ContainerFactory() {
         @Override
@@ -24,6 +25,7 @@ public class ModularStorageItemContainer extends GenericContainer {
         this.entityPlayer = player;
         addInventory(CONTAINER_INVENTORY, new ModularStorageItemInventory(player));
         addInventory(ContainerFactory.CONTAINER_PLAYER, player.inventory);
+        tabletIndex = player.inventory.currentItem;
         generateSlots();
     }
 
@@ -35,7 +37,19 @@ public class ModularStorageItemContainer extends GenericContainer {
     public void generateSlots() {
         for (SlotFactory slotFactory : factory.getSlots()) {
             Slot slot;
-            if (slotFactory.getSlotType() == SlotType.SLOT_PLAYERINV || slotFactory.getSlotType() == SlotType.SLOT_PLAYERHOTBAR) {
+            if (slotFactory.getSlotType() == SlotType.SLOT_PLAYERHOTBAR) {
+                if (slotFactory.getIndex() == tabletIndex) {
+                    slot = new BaseSlot(inventories.get(slotFactory.getInventoryName()), slotFactory.getIndex(), slotFactory.getX(), slotFactory.getY()) {
+                        @Override
+                        public boolean canTakeStack(EntityPlayer player) {
+                            // We don't want to take the stack from this slot.
+                            return false;
+                        }
+                    };
+                } else {
+                    slot = new BaseSlot(inventories.get(slotFactory.getInventoryName()), slotFactory.getIndex(), slotFactory.getX(), slotFactory.getY());
+                }
+            } else if (slotFactory.getSlotType() == SlotType.SLOT_PLAYERINV) {
                 slot = new BaseSlot(inventories.get(slotFactory.getInventoryName()), slotFactory.getIndex(), slotFactory.getX(), slotFactory.getY());
             } else {
                 slot = new BaseSlot(inventories.get(slotFactory.getInventoryName()), slotFactory.getIndex(), slotFactory.getX(), slotFactory.getY()) {
