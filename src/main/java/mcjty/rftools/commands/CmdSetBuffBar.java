01907a545d6fa8ef62e0bb7fe3723af5e4616fd6
McJty <mcjty1@gmail.com>
2015-06-16 15:58:05 +0200

    Added an option to display current buff bar position

--- src/main/java/mcjty/rftools/commands/CmdSetBuffBar.java
+++ src/main/java/mcjty/rftools/commands/CmdSetBuffBar.java
@@ -9,7 +9,7 @@ import net.minecraft.util.EnumChatFormatting;
 public class CmdSetBuffBar extends AbstractRfToolsCommand {
     @Override
     public String getHelp() {
-        return "<x> <y>";
+        return "[<x> <y>]";
     }
 
     @Override
@@ -22,33 +22,30 @@ public class CmdSetBuffBar extends AbstractRfToolsCommand {
         return 0;
     }
 
-//    @Override
-//    public boolean isClientSide() {
-//        return true;
-//    }
-
     @Override
     public void execute(ICommandSender sender, String[] args) {
-        if (args.length < 3) {
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "The coordinate parameters are missing!"));
-            return;
-        } else if (args.length > 3) {
+        if (args.length > 3) {
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Too many parameters!"));
+            return;
+        }
+
+        if (!(sender instanceof EntityPlayer)) {
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "This command only works as a player!"));
+            return;
+        }
+
+        EntityPlayer player = (EntityPlayer) sender;
+        PlayerExtendedProperties properties = PlayerExtendedProperties.getProperties(player);
+
+        if (args.length < 3) {
+            int buffX = properties.getPreferencesProperties().getBuffX();
+            int buffY = properties.getPreferencesProperties().getBuffY();
+            ((EntityPlayer) sender).addChatComponentMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "Current buffbar location: " + buffX + "," + buffY));
             return;
         }
 
         int x = fetchInt(sender, args, 1, 0);
         int y = fetchInt(sender, args, 2, 0);
-
-        if (sender instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) sender;
-
-            PlayerExtendedProperties properties = PlayerExtendedProperties.getProperties(player);
-            properties.getPreferencesProperties().setBuffXY(x, y);
-//            Preferences.setBuffBarX(x);
-//            Preferences.setBuffBarY(y);
-        } else {
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "This command only works as a player!"));
-        }
+        properties.getPreferencesProperties().setBuffXY(x, y);
     }
 }
