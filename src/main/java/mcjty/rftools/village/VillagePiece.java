a9f8fc97269bb5c5b7859bf7e3e4c064884513fd
McJty <mcjty1@gmail.com>
2015-06-26 13:40:21 +0200

    Made the RFTools village house a bit more RFTools-like (contains two working sequencers now)

--- src/main/java/mcjty/rftools/village/VillagePiece.java
+++ src/main/java/mcjty/rftools/village/VillagePiece.java
@@ -1,9 +1,15 @@
 package mcjty.rftools.village;
 
 import mcjty.rftools.GeneralConfiguration;
+import mcjty.rftools.blocks.dimlets.DimletSetup;
+import mcjty.rftools.blocks.logic.LogicBlockSetup;
+import mcjty.rftools.blocks.logic.SequencerMode;
+import mcjty.rftools.blocks.logic.SequencerTileEntity;
+import mcjty.varia.Coordinate;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraft.world.gen.structure.StructureComponent;
@@ -17,15 +23,105 @@ public class VillagePiece extends StructureVillagePieces.Village {
     public VillagePiece() {
     }
 
-    public VillagePiece(StructureVillagePieces.Start p_i2094_1_, int p_i2094_2_, Random p_i2094_3_, StructureBoundingBox p_i2094_4_, int p_i2094_5_) {
-        super(p_i2094_1_, p_i2094_2_);
-        this.coordBaseMode = p_i2094_5_;
-        this.boundingBox = p_i2094_4_;
+    public VillagePiece(StructureVillagePieces.Start start, int weight, Random random, StructureBoundingBox box, int coordBaseMode) {
+        super(start, weight);
+        this.coordBaseMode = coordBaseMode;
+        this.boundingBox = box;
     }
 
     public static VillagePiece buildPiece(StructureVillagePieces.Start start, List list, Random random, int p_74898_3_, int p_74898_4_, int p_74898_5_, int p_74898_6_, int p_74898_7_) {
         StructureBoundingBox structureboundingbox = StructureBoundingBox.getComponentToAddBoundingBox(p_74898_3_, p_74898_4_, p_74898_5_, 0, 0, 0, 9, 9, 6, p_74898_6_);
         return canVillageGoDeeper(structureboundingbox) && StructureComponent.findIntersecting(list, structureboundingbox) == null ? new VillagePiece(start, p_74898_7_, random, structureboundingbox, p_74898_6_) : null;
+    }
+
+
+    /**
+     * arguments: (World worldObj, StructureBoundingBox structBB, int minX, int minY, int minZ, int maxX, int maxY, int
+     * maxZ, Block placeBlock, int placeBlockMetadata, Block replaceBlock, int replaceBlockMetadata, boolean
+     * alwaysreplace)
+     */
+    protected void fillWithMetadataBlocks(World world, StructureBoundingBox box, int minX, int minY, int minZ, int maxX, int maxY, int maxZ, Block block, int meta) {
+        for (int i2 = minY; i2 <= maxY; ++i2) {
+            for (int j2 = minX; j2 <= maxX; ++j2) {
+                for (int k2 = minZ; k2 <= maxZ; ++k2) {
+                    this.placeBlockAtCurrentPosition(world, block, meta, j2, i2, k2, box);
+                }
+            }
+        }
+    }
+
+    /**
+     * arguments: (World worldObj, StructureBoundingBox structBB, int minX, int minY, int minZ, int maxX, int
+     * maxY, int maxZ, int placeBlock, int replaceBlock, boolean alwaysreplace)
+     */
+    private void fillWithBlocks(World world, StructureBoundingBox box, int minX, int minY, int minZ, int maxX, int maxY, int maxZ, Block placeBlock, int meta) {
+        fillWithMetadataBlocks(world, box, minX, minY, minZ, maxX, maxY, maxZ, placeBlock, meta);
+    }
+
+    /**
+     * Current Position depends on currently set Coordinates mode, is computed here
+     */
+    private Coordinate getCoordinate(int x, int y, int z) {
+        int i1 = this.getXWithOffset(x, z);
+        int j1 = this.getYWithOffset(y);
+        int k1 = this.getZWithOffset(x, z);
+        return new Coordinate(i1, j1, k1);
+    }
+
+    private void setupSequencer(World world, int x, int y, int z, Random random) {
+        Coordinate c = getCoordinate(x, y, z);
+        TileEntity tileEntity = world.getTileEntity(c.getX(), c.getY(), c.getZ());
+        if (tileEntity instanceof SequencerTileEntity) {
+            SequencerTileEntity sequencerTileEntity = (SequencerTileEntity) tileEntity;
+
+            sequencerTileEntity.setDelay(5);
+            for (int i = 0; i < 64; i++) {
+                sequencerTileEntity.setCycleBit(i, random.nextBoolean());
+            }
+            sequencerTileEntity.setMode(SequencerMode.MODE_LOOP1);
+        }
+    }
+
+    private int rotateMetaSeq(int meta) {
+        switch (coordBaseMode) {
+            case 3:
+                return meta;
+            case 0:
+                switch (meta) {
+                    case 3: return 1;
+                    case 2: return 4;
+                }
+                break;
+            case 2:
+                switch (meta) {
+                    case 3: return 0;
+                    case 2: return 5;
+                }
+                break;
+            case 1:
+                switch (meta) {
+                    case 3: return 2;
+                    case 2: return 3;
+                }
+                break;
+        }
+        return meta;
+    }
+
+    private int rotateMeta(int meta) {
+        switch (coordBaseMode) {
+            case 3:
+                return meta;
+            case 0:
+            case 2:
+                switch (meta) {
+                    case 3: return 0;
+                    case 4: return 2;
+                    case 2: return 4;
+                }
+                break;
+        }
+        return meta;
     }
 
     /**
@@ -44,13 +140,21 @@ public class VillagePiece extends StructureVillagePieces.Village {
             this.boundingBox.offset(0, this.field_143015_k - this.boundingBox.maxY + 9 - 1, 0);
         }
 
-        this.fillWithBlocks(world, bbox, 1, 1, 1, 7, 5, 4, Blocks.air, Blocks.air, false);
-        Block stone = Blocks.hardened_clay;
-//        Block stone = Blocks.cobblestone;
-        this.fillWithBlocks(world, bbox, 0, 0, 0, 8, 0, 5, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 0, 5, 0, 8, 5, 5, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 0, 6, 1, 8, 6, 4, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 0, 7, 2, 8, 7, 3, stone, stone, false);
+        this.fillWithBlocks(world, bbox, 1, 1, 1, 7, 5, 4, Blocks.air, 0);
+        Block stone = Blocks.cobblestone;
+        int meta = 0;
+//        Block stone = Blocks.stained_hardened_clay;
+//        int meta = 3;
+
+        this.fillWithBlocks(world, bbox, 0, 0, 0, 0, 0, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 1, 0, 5, 8, 0, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 8, 0, 0, 8, 0, 4, stone, meta);
+        this.fillWithBlocks(world, bbox, 1, 0, 0, 7, 0, 0, stone, meta);
+
+        this.fillWithBlocks(world, bbox, 1, 0, 1, 7, 0, 4, DimletSetup.dimensionalBlankBlock, 0);
+        this.fillWithBlocks(world, bbox, 0, 5, 0, 8, 5, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 0, 6, 1, 8, 6, 4, stone, meta);
+        this.fillWithBlocks(world, bbox, 0, 7, 2, 8, 7, 3, stone, meta);
         int i = this.getMetadataWithOffset(Blocks.oak_stairs, 3);
         int j = this.getMetadataWithOffset(Blocks.oak_stairs, 2);
         int k;
@@ -63,18 +167,18 @@ public class VillagePiece extends StructureVillagePieces.Village {
             }
         }
 
-        this.fillWithBlocks(world, bbox, 0, 1, 0, 0, 1, 5, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 1, 1, 5, 8, 1, 5, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 8, 1, 0, 8, 1, 4, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 2, 1, 0, 7, 1, 0, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 0, 2, 0, 0, 4, 0, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 0, 2, 5, 0, 4, 5, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 8, 2, 5, 8, 4, 5, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 8, 2, 0, 8, 4, 0, stone, stone, false);
-        this.fillWithBlocks(world, bbox, 0, 2, 1, 0, 4, 4, Blocks.planks, Blocks.planks, false);
-        this.fillWithBlocks(world, bbox, 1, 2, 5, 7, 4, 5, Blocks.planks, Blocks.planks, false);
-        this.fillWithBlocks(world, bbox, 8, 2, 1, 8, 4, 4, Blocks.planks, Blocks.planks, false);
-        this.fillWithBlocks(world, bbox, 1, 2, 0, 7, 4, 0, Blocks.planks, Blocks.planks, false);
+        this.fillWithBlocks(world, bbox, 0, 1, 0, 0, 1, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 1, 1, 5, 8, 1, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 8, 1, 0, 8, 1, 4, stone, meta);
+        this.fillWithBlocks(world, bbox, 2, 1, 0, 7, 1, 0, stone, meta);
+        this.fillWithBlocks(world, bbox, 0, 2, 0, 0, 4, 0, stone, meta);
+        this.fillWithBlocks(world, bbox, 0, 2, 5, 0, 4, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 8, 2, 5, 8, 4, 5, stone, meta);
+        this.fillWithBlocks(world, bbox, 8, 2, 0, 8, 4, 0, stone, meta);
+        this.fillWithBlocks(world, bbox, 0, 2, 1, 0, 4, 4, Blocks.planks, 0);
+        this.fillWithBlocks(world, bbox, 1, 2, 5, 7, 4, 5, Blocks.planks, 0);
+        this.fillWithBlocks(world, bbox, 8, 2, 1, 8, 4, 4, Blocks.planks, 0);
+        this.fillWithBlocks(world, bbox, 1, 2, 0, 7, 4, 0, Blocks.planks, 0);
         this.placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, 4, 2, 0, bbox);
         this.placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, 5, 2, 0, bbox);
         this.placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, 6, 2, 0, bbox);
@@ -93,21 +197,27 @@ public class VillagePiece extends StructureVillagePieces.Village {
         this.placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, 3, 2, 5, bbox);
         this.placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, 5, 2, 5, bbox);
         this.placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, 6, 2, 5, bbox);
-        this.fillWithBlocks(world, bbox, 1, 4, 1, 7, 4, 1, Blocks.planks, Blocks.planks, false);
-        this.fillWithBlocks(world, bbox, 1, 4, 4, 7, 4, 4, Blocks.planks, Blocks.planks, false);
-        this.fillWithBlocks(world, bbox, 1, 3, 4, 7, 3, 4, Blocks.bookshelf, Blocks.bookshelf, false);
-        this.placeBlockAtCurrentPosition(world, Blocks.planks, 0, 7, 1, 4, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.oak_stairs, this.getMetadataWithOffset(Blocks.oak_stairs, 0), 7, 1, 3, bbox);
-        k = this.getMetadataWithOffset(Blocks.oak_stairs, 3);
-        this.placeBlockAtCurrentPosition(world, Blocks.oak_stairs, k, 6, 1, 4, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.oak_stairs, k, 5, 1, 4, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.oak_stairs, k, 4, 1, 4, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.oak_stairs, k, 3, 1, 4, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.fence, 0, 6, 1, 3, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.wooden_pressure_plate, 0, 6, 2, 3, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.fence, 0, 4, 1, 3, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.wooden_pressure_plate, 0, 4, 2, 3, bbox);
-        this.placeBlockAtCurrentPosition(world, Blocks.crafting_table, 0, 7, 1, 1, bbox);
+        this.fillWithBlocks(world, bbox, 1, 4, 1, 7, 4, 1, Blocks.planks, 0);
+        this.fillWithBlocks(world, bbox, 1, 4, 4, 7, 4, 4, Blocks.planks, 0);
+
+        this.placeBlockAtCurrentPosition(world, LogicBlockSetup.sequencerBlock, rotateMetaSeq(3), 7, 1, 4, bbox);      // 3->3, 2->0
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, 0, 7, 2, 4, bbox);
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, 0, 7, 3, 4, bbox);
+        this.placeBlockAtCurrentPosition(world, Blocks.redstone_lamp, 0, 7, 1, 3, bbox);
+        this.placeBlockAtCurrentPosition(world, Blocks.redstone_lamp, 0, 7, 1, 2, bbox);
+        this.placeBlockAtCurrentPosition(world, LogicBlockSetup.sequencerBlock, rotateMetaSeq(2), 7, 1, 1, bbox);      // 3->2, 2->5
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, 0, 7, 2, 1, bbox);
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, 0, 7, 3, 1, bbox);
+
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, rotateMeta(4), 7, 4, 2, bbox);  // 3->4, 2->2
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, rotateMeta(4), 7, 4, 3, bbox);  // 3->4, 2->2
+
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, rotateMeta(2), 6, 1, 4, bbox);  // 3->2, 2->4
+        this.placeBlockAtCurrentPosition(world, DimletSetup.dimensionalCross2Block, rotateMeta(2), 6, 1, 1, bbox);  // 3->2, 2->4
+
+        setupSequencer(world, 7, 1, 4, random);
+        setupSequencer(world, 7, 1, 1, random);
+
         this.placeBlockAtCurrentPosition(world, Blocks.air, 0, 1, 1, 0, bbox);
         this.placeBlockAtCurrentPosition(world, Blocks.air, 0, 1, 2, 0, bbox);
         this.placeDoorAtCurrentPosition(world, bbox, random, 1, 1, 0, this.getMetadataWithOffset(Blocks.wooden_door, 1));
