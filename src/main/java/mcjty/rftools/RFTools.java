3ce48c93478e8dec0282449d373a82de4fc1b84d
McJty <mcjty1@gmail.com>
2015-11-20 06:37:23 +0100

    Version 4.12 and dependency on McJtyLib 1.8.0

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -51,10 +51,10 @@ import java.util.EnumMap;
         version = RFTools.VERSION)
 public class RFTools implements ModBase {
     public static final String MODID = "rftools";
-    public static final String VERSION = "4.11";
+    public static final String VERSION = "4.12";
     public static final String MIN_FORGE_VER = "10.13.2.1291";
     public static final String MIN_COFHLIB_VER = "1.0.3";
-    public static final String MIN_MCJTYLIB_VER = "1.7.0";
+    public static final String MIN_MCJTYLIB_VER = "1.8.0";
 
     @SidedProxy(clientSide="mcjty.rftools.ClientProxy", serverSide="mcjty.rftools.ServerProxy")
     public static CommonProxy proxy;
