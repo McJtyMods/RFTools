d5b216f9ea78744b50aa56eaa0cfd818632d27c3
McJty <mcjty1@gmail.com>
2015-12-17 09:05:50 +0100

    More work on the coal generator

---
+++ src/main/java/mcjty/rftools/blocks/generator/CoalGeneratorContainer.java
@@ -0,0 +1,30 @@
+package mcjty.rftools.blocks.generator;
+
+import mcjty.lib.container.ContainerFactory;
+import mcjty.lib.container.GenericContainer;
+import mcjty.lib.container.SlotDefinition;
+import mcjty.lib.container.SlotType;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class CoalGeneratorContainer extends GenericContainer {
+    public static final String CONTAINER_INVENTORY = "container";
+
+    public static final int SLOT_COALINPUT = 0;
+
+    public static final ContainerFactory factory = new ContainerFactory() {
+        @Override
+        protected void setup() {
+            addSlotBox(new SlotDefinition(SlotType.SLOT_SPECIFICITEM, new ItemStack(Items.coal)), CONTAINER_INVENTORY, SLOT_COALINPUT, 64, 24, 1, 18, 1, 18);
+            layoutPlayerInventorySlots(10, 70);
+        }
+    };
+
+    public CoalGeneratorContainer(EntityPlayer player, CoalGeneratorTileEntity containerInventory) {
+        super(factory);
+        addInventory(CONTAINER_INVENTORY, containerInventory);
+        addInventory(ContainerFactory.CONTAINER_PLAYER, player.inventory);
+        generateSlots();
+    }
+}
--- src/main/java/mcjty/rftools/blocks/generator/CoalGeneratorTileEntity.java
+++ src/main/java/mcjty/rftools/blocks/generator/CoalGeneratorTileEntity.java
@@ -1,10 +1,18 @@
 package mcjty.rftools.blocks.generator;
 
 
+import mcjty.lib.container.InventoryHelper;
 import mcjty.lib.entity.GenericEnergyProviderTileEntity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.util.ITickable;
 
-public class CoalGeneratorTileEntity extends GenericEnergyProviderTileEntity implements ITickable {
+public class CoalGeneratorTileEntity extends GenericEnergyProviderTileEntity implements ITickable, ISidedInventory {
+
+    private InventoryHelper inventoryHelper = new InventoryHelper(this, CoalGeneratorContainer.factory, 1);
 
     public CoalGeneratorTileEntity() {
         super(1000000, 2000);
@@ -13,7 +21,106 @@ public class CoalGeneratorTileEntity extends GenericEnergyProviderTileEntity imp
     @Override
     public void update() {
         if (!worldObj.isRemote) {
-
         }
+    }
+
+    @Override
+    public int[] getSlotsForFace(EnumFacing side) {
+        return new int[] { CoalGeneratorContainer.SLOT_COALINPUT };
+    }
+
+    @Override
+    public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
+        return true;
+    }
+
+    @Override
+    public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) {
+        return false;
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return inventoryHelper.getCount();
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        return inventoryHelper.getStackInSlot(index);
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        return inventoryHelper.decrStackSize(index, count);
+    }
+
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+        return inventoryHelper.removeStackFromSlot(index);
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        inventoryHelper.setInventorySlotContents(64, index, stack);
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return canPlayerAccess(player);
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {
+
+    }
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "coalgenerator";
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return false;
+    }
+
+    @Override
+    public IChatComponent getDisplayName() {
+        return null;
     }
 }
