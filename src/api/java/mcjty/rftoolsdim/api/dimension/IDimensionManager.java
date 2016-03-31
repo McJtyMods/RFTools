ddfb7e3f894c81eee06e61db1b3e23d93fb4d2b5
McJty <mcjty1@gmail.com>
2016-03-31 07:24:07 +0200

    Updated the dimension api

--- src/api/java/mcjty/rftoolsdim/api/dimension/IDimensionManager.java
+++ src/api/java/mcjty/rftoolsdim/api/dimension/IDimensionManager.java
@@ -1,5 +1,7 @@
 package mcjty.rftoolsdim.api.dimension;
 
+import net.minecraft.world.World;
+
 /**
  * Get a reference to an implementation of this interface by calling:
  *         FMLInterModComms.sendFunctionMessage("rftoolsdim", "getDimensionManager", "<whatever>.YourClass$GetDimensionManager");
@@ -9,26 +11,29 @@ public interface IDimensionManager {
     /**
      * Return true if this dimension represents an RFTools dimension.
      * Only call this server-side
+     * @param world is a reference to any world (overworld is fine)
      * @param id
      * @return
      */
-    boolean isRFToolsDimension(int id);
+    boolean isRFToolsDimension(World world, int id);
 
     /**
      * Return the power left in the current dimension. Returns -1 if it is not
      * an RFTools dimension
      * Only call this server-side
+     * @param world is a reference to any world (overworld is fine)
      * @param id
      * @return
      */
-    int getCurrentRF(int id);
+    int getCurrentRF(World world, int id);
 
     /**
      * Get the dimension information. If the id doesn't represent an RFTools
      * dimension this just returns null.
      * Only call this server-side
+     * @param world is a reference to any world (overworld is fine)
      * @param id
      * @return
      */
-    IDimensionInformation getDimensionInformation(int id);
+    IDimensionInformation getDimensionInformation(World world, int id);
 }
