3b4e4cd0ef96188695f9de68ea2e5b5aa8aebad9
McJty <mcjty1@gmail.com>
2016-04-29 15:05:23 +0200

    4.23beta38

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -36,7 +36,7 @@ import java.util.EnumMap;
         version = RFTools.VERSION)
 public class RFTools implements ModBase {
     public static final String MODID = "rftools";
-    public static final String VERSION = "4.23beta37";
+    public static final String VERSION = "4.23beta38";
     public static final String MIN_FORGE_VER = "11.15.1.1722";
 //    public static final String MIN_COFHLIB_VER = "1.0.3";
     public static final String MIN_MCJTYLIB_VER = "1.8.9-1.8.1beta10";
