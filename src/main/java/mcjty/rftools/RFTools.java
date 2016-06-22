e619de18225536d11c1c9debc55786895dc530f5
McJty <mcjty1@gmail.com>
2016-06-22 13:56:02 +0200

    4.65

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -35,7 +35,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
         version = RFTools.VERSION)
 public class RFTools implements ModBase {
     public static final String MODID = "rftools";
-    public static final String VERSION = "4.64";
+    public static final String VERSION = "4.65";
     public static final String MIN_FORGE_VER = "12.16.1.1898";
     public static final String MIN_MCJTYLIB_VER = "1.9.4-1.9.0";
 
