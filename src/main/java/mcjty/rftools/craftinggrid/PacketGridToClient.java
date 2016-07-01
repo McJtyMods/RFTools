4fcefa6abd54785bc1410ff2e51af4005c2af910
McJty <mcjty1@gmail.com>
2016-07-01 16:35:30 +0200

    Probably fixed a server side WorldClient dependency

--- src/main/java/mcjty/rftools/craftinggrid/PacketGridToClient.java
+++ src/main/java/mcjty/rftools/craftinggrid/PacketGridToClient.java
@@ -2,7 +2,9 @@ package mcjty.rftools.craftinggrid;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -29,7 +31,8 @@ public class PacketGridToClient extends PacketGridSync implements IMessage {
     public static class Handler implements IMessageHandler<PacketGridToClient, IMessage> {
         @Override
         public IMessage onMessage(PacketGridToClient message, MessageContext ctx) {
-            Minecraft.getMinecraft().addScheduledTask(() -> message.handleMessage(Minecraft.getMinecraft().theWorld));
+            World world = Minecraft.getMinecraft().theWorld;
+            Minecraft.getMinecraft().addScheduledTask(() -> message.handleMessage(world));
             return null;
         }
     }
