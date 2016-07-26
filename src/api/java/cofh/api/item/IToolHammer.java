63f4dd3a1eade6091c715783d4b5e9ce174e065a
McJty <mcjty1@gmail.com>
2016-07-26 10:06:45 +0200

    Fixes for IToolHammer API

--- src/api/java/cofh/api/item/IToolHammer.java
+++ src/api/java/cofh/api/item/IToolHammer.java
@@ -1,7 +1,9 @@
 package cofh.api.item;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 
 /**
  * Implement this interface on subclasses of Item to have that item work as a tool for CoFH mods.
@@ -11,34 +13,38 @@ public interface IToolHammer {
     /**
      * Called to ensure that the wrench can be used.
      *
-     * @param item
-     *            The itemstack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
-     * @param user
-     *            The entity using the tool
-     * @param x
-     *            X location of the block/tile
-     * @param y
-     *            Y location of the block/tile
-     * @param z
-     *            Z location of the block/tile
+     * @param item The itemstack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
+     * @param user The entity using the tool
+     * @param pos  Coordinates of the block
      * @return True if this tool can be used
      */
-    boolean isUsable(ItemStack item, EntityLivingBase user, int x, int y, int z);
+    boolean isUsable(ItemStack item, EntityLivingBase user, BlockPos pos);
+
+    /**
+     * Called to ensure that the tool can be used on an entity.
+     *
+     * @param item   The ItemStack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools).
+     * @param user   The entity using the tool.
+     * @param entity The entity the tool is being used on.
+     * @return True if this tool can be used.
+     */
+    boolean isUsable(ItemStack item, EntityLivingBase user, Entity entity);
 
     /**
      * Callback for when the tool has been used reactively.
      *
-     * @param item
-     *            The ItemStack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools)
-     * @param user
-     *            The entity using the tool
-     * @param x
-     *            X location of the block/tile
-     * @param y
-     *            Y location of the block/tile
-     * @param z
-     *            Z location of the block/tile
+     * @param item The ItemStack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools).
+     * @param user The entity using the tool.
+     * @param pos  Coordinates of the block.
      */
-    void toolUsed(ItemStack item, EntityLivingBase user, int x, int y, int z);
+    void toolUsed(ItemStack item, EntityLivingBase user, BlockPos pos);
 
+    /**
+     * Callback for when the tool has been used reactively.
+     *
+     * @param item   The ItemStack for the tool. Not required to match equipped item (e.g., multi-tools that contain other tools).
+     * @param user   The entity using the tool.
+     * @param entity The entity the tool is being used on.
+     */
+    void toolUsed(ItemStack item, EntityLivingBase user, Entity entity);
 }
