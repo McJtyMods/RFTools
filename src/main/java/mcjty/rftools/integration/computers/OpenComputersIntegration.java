9c105dadfdfa1aae36b3ab477df1493a48859080
Martin Zeller <mz.bremerhaven@gmail.com>
2016-11-21 03:40:39 +0100

    OpenComputers integration framework

---
+++ src/main/java/mcjty/rftools/integration/computers/OpenComputersIntegration.java
@@ -0,0 +1,10 @@
+package mcjty.rftools.integration.computers;
+
+import li.cil.oc.api.Driver;
+import net.minecraftforge.fml.common.Optional;
+
+public class OpenComputersIntegration {
+    @Optional.Method(modid="OpenComputers")
+    public static void init() {
+    }
+}
