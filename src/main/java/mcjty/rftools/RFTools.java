bd355d5132d62670de555f17290a937d048e9a7a
McJty <mcjty1@gmail.com>
2016-12-04 11:59:04 +0100

    Use mcjtylib beta3

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -45,7 +45,7 @@ public class RFTools implements ModBase {
     public static final String VERSION = "5.62beta1";
     public static final String MIN_FORGE10_VER = "12.18.1.2082";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "2.2.0beta2";
+    public static final String MIN_MCJTYLIB_VER = "2.2.0beta3";
     public static final String COMPATLAYER_VER = "0.0.40beta";
 
     @SidedProxy(clientSide = "mcjty.rftools.proxy.ClientProxy", serverSide = "mcjty.rftools.proxy.ServerProxy")
