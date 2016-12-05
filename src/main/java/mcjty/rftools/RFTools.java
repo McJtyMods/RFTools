88655454f863d57b86912d7a9465547ca85ac72e
McJty <mcjty1@gmail.com>
2016-12-05 11:22:20 +0100

    5.70

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -42,11 +42,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
         acceptedMinecraftVersions = "[1.10,1.12)")
 public class RFTools implements ModBase {
     public static final String MODID = "rftools";
-    public static final String VERSION = "5.62beta2";
+    public static final String VERSION = "5.70";
     public static final String MIN_FORGE10_VER = "12.18.1.2082";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "2.2.0beta3";
-    public static final String COMPATLAYER_VER = "0.0.40beta";
+    public static final String MIN_MCJTYLIB_VER = "2.3.0";
+    public static final String COMPATLAYER_VER = "0.1.0";
 
     @SidedProxy(clientSide = "mcjty.rftools.proxy.ClientProxy", serverSide = "mcjty.rftools.proxy.ServerProxy")
     public static CommonProxy proxy;
