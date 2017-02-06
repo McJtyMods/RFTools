3301cc8cfec7c21c3586e060cfb37a93c776b868
McJty <mcjty1@gmail.com>
2017-02-06 10:04:36 +0100

    Added debug keys to dump NBT for items and blocks

--- src/main/java/mcjty/rftools/keys/KeyInputHandler.java
+++ src/main/java/mcjty/rftools/keys/KeyInputHandler.java
@@ -1,7 +1,11 @@
 package mcjty.rftools.keys;
 
+import mcjty.lib.debugtools.DumpBlockNBT;
+import mcjty.lib.debugtools.DumpItemNBT;
+import mcjty.lib.tools.MinecraftTools;
 import mcjty.rftools.items.teleportprobe.PacketCycleDestination;
 import mcjty.rftools.network.RFToolsMessages;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
@@ -13,6 +17,10 @@ public class KeyInputHandler {
             RFToolsMessages.INSTANCE.sendToServer(new PacketCycleDestination(true));
         } else if (KeyBindings.porterPrevDestination.isPressed()) {
             RFToolsMessages.INSTANCE.sendToServer(new PacketCycleDestination(false));
+        } else if (KeyBindings.debugDumpNBTItem.isPressed()) {
+            DumpItemNBT.dumpHeldItem(RFToolsMessages.INSTANCE, MinecraftTools.getPlayer(Minecraft.getMinecraft()), false);
+        } else if (KeyBindings.debugDumpNBTBlock.isPressed()) {
+            DumpBlockNBT.dumpFocusedBlock(RFToolsMessages.INSTANCE, MinecraftTools.getPlayer(Minecraft.getMinecraft()), true, false);
         }
     }
 }
