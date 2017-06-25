bcfdbe6433cdeebf71de91c97bc4003183e80c9e
McJty <mcjty1@gmail.com>
2017-06-25 06:19:27 +0200

    Update requirements

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -42,7 +42,7 @@ public class RFTools implements ModBase {
     public static final String VERSION = "6.03";
     public static final String MIN_FORGE11_VER = "13.20.0.2266";
     public static final String MIN_MCJTYLIB_VER = "2.3.13";
-    public static final String MIN_XNET_VER = "1.3.2";
+    public static final String MIN_XNET_VER = "1.3.3";
 
     @SidedProxy(clientSide = "mcjty.rftools.proxy.ClientProxy", serverSide = "mcjty.rftools.proxy.ServerProxy")
     public static CommonProxy proxy;
