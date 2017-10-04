c963603732c6fd74d6a1a78b7ae1c5b63ae5822a
McJty <mcjty1@gmail.com>
2017-10-04 16:25:41 +0200

    Advanced projector animation control

--- src/main/java/mcjty/rftools/blocks/shaper/GuiProjector.java
+++ src/main/java/mcjty/rftools/blocks/shaper/GuiProjector.java
@@ -6,26 +6,21 @@ import mcjty.lib.gui.Window;
 import mcjty.lib.gui.WindowManager;
 import mcjty.lib.gui.layout.HorizontalAlignment;
 import mcjty.lib.gui.layout.PositionalLayout;
-import mcjty.lib.gui.widgets.Button;
 import mcjty.lib.gui.widgets.*;
-import mcjty.lib.gui.widgets.Label;
-import mcjty.lib.gui.widgets.Panel;
-import mcjty.lib.gui.widgets.TextField;
 import mcjty.lib.network.Argument;
 import mcjty.lib.tools.ItemStackTools;
-import mcjty.lib.varia.RedstoneMode;
 import mcjty.rftools.RFTools;
-import mcjty.rftools.items.builder.ShapeCardItem;
 import mcjty.rftools.network.RFToolsMessages;
 import mcjty.rftools.shapes.IShapeParentGui;
-import mcjty.rftools.shapes.ShapeID;
 import mcjty.rftools.shapes.ShapeRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.input.Mouse;
 
-import java.awt.*;
+import java.awt.Rectangle;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> implements IShapeParentGui {
 
@@ -38,7 +33,6 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
     private static final ResourceLocation iconGuiElements = new ResourceLocation(RFTools.MODID, "textures/gui/guielements.png");
 
     private EnergyBar energyBar;
-    private ImageChoiceLabel redstoneMode;
 
     private ToggleButton showAxis;
     private ToggleButton showOuter;
@@ -51,8 +45,10 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
     private Slider scaleSlider;
     private ToggleButton autoRotate;
 
-    private ChoiceLabel[] rsLabel;
-    private TextField[] val;
+    private ChoiceLabel[] rsLabelOn = new ChoiceLabel[4];
+    private ChoiceLabel[] rsLabelOff = new ChoiceLabel[4];
+    private TextField[] valOn = new TextField[4];
+    private TextField[] valOff = new TextField[4];
 
     private Window sideWindow;
 
@@ -67,20 +63,10 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
 
     private ShapeRenderer getShapeRenderer() {
         if (shapeRenderer == null) {
-            shapeRenderer = new ShapeRenderer(getShapeID());
+            shapeRenderer = new ShapeRenderer(tileEntity.getShapeID());
         }
         return shapeRenderer;
     }
-
-    private ShapeID getShapeID() {
-        int scanId = ShapeCardItem.getScanId(tileEntity.getRenderStack());
-        if (scanId == 0) {
-            return new ShapeID(tileEntity.getWorld().provider.getDimension(), tileEntity.getPos(), scanId);
-        } else {
-            return new ShapeID(0, null, scanId);
-        }
-    }
-
 
     @Override
     public void initGui() {
@@ -91,12 +77,9 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
         Panel toplevel = new Panel(mc, this).setBackground(iconLocation).setLayout(new PositionalLayout());
 
         int maxEnergyStored = tileEntity.getMaxEnergyStored();
-        energyBar = new EnergyBar(mc, this).setHorizontal().setMaxValue(maxEnergyStored).setLayoutHint(new PositionalLayout.PositionalHint(26, 184, 55, 10)).setShowText(false);
+        energyBar = new EnergyBar(mc, this).setHorizontal().setMaxValue(maxEnergyStored).setLayoutHint(new PositionalLayout.PositionalHint(6, 184, 75, 10)).setShowText(false);
         energyBar.setValue(GenericEnergyStorageTileEntity.getCurrentRF());
         toplevel.addChild(energyBar);
-
-        initRedstoneMode();
-        toplevel.addChild(redstoneMode);
 
         Label angleI = new Label(mc, this).setText("Angle:");
         angleI.setLayoutHint(new PositionalLayout.PositionalHint(23, 30, 18, 15));
@@ -168,19 +151,84 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
 
     private void initSidePanel() {
         Panel sidePanel = new Panel(mc, this).setLayout(new PositionalLayout()).setBackground(sideBackground);
-        sidePanel.addChild(new Label<>(mc, this).setText("North").setHorizontalAlignment(HorizontalAlignment.ALIGH_LEFT).setLayoutHint(new PositionalLayout.PositionalHint(8, 10, 50, 13)));
-        sidePanel.addChild(new Label<>(mc, this).setText("South").setHorizontalAlignment(HorizontalAlignment.ALIGH_LEFT).setLayoutHint(new PositionalLayout.PositionalHint(8, 63, 50, 13)));
-        sidePanel.addChild(new Label<>(mc, this).setText("East").setHorizontalAlignment(HorizontalAlignment.ALIGH_LEFT).setLayoutHint(new PositionalLayout.PositionalHint(8, 116, 50, 13)));
-        sidePanel.addChild(new Label<>(mc, this).setText("West").setHorizontalAlignment(HorizontalAlignment.ALIGH_LEFT).setLayoutHint(new PositionalLayout.PositionalHint(8, 169, 50, 13)));
+        initRsPanel(sidePanel, 0, "N");
+        initRsPanel(sidePanel, 1, "S");
+        initRsPanel(sidePanel, 2, "W");
+        initRsPanel(sidePanel, 3, "E");
         sidePanel.setBounds(new Rectangle(guiLeft-SIDEWIDTH, guiTop, SIDEWIDTH, ySize));
         sideWindow = new Window(this, sidePanel);
     }
 
-    private void initRsPanel(Panel sidePanel, int o, String label, int y) {
-        sidePanel.addChild(new Label<>(mc, this).setText(label).setHorizontalAlignment(HorizontalAlignment.ALIGH_LEFT).setLayoutHint(new PositionalLayout.PositionalHint(8, y+10, 50, 13)));
-        rsLabel[o] = new ChoiceLabel(mc, this);
-        sidePanel.addChild(rsLabel[o]);
+    private void initRsPanel(Panel sidePanel, int o, String label) {
+        int dy = o * 53;
+        sidePanel.addChild(new Label<>(mc, this)
+                .setText(label)
+                .setHorizontalAlignment(HorizontalAlignment.ALIGH_LEFT)
+                .setLayoutHint(new PositionalLayout.PositionalHint(8, dy+8, 12, 13)));
 
+        ImageLabel redstoneOn = new ImageLabel(mc, this).setImage(iconGuiElements, 16, 96);
+        redstoneOn.setLayoutHint(new PositionalLayout.PositionalHint(20, dy+8, 16, 16));
+        ImageLabel redstoneOff = new ImageLabel(mc, this).setImage(iconGuiElements, 0, 96);
+        redstoneOff.setLayoutHint(new PositionalLayout.PositionalHint(54, dy+8, 16, 16));
+        sidePanel.addChild(redstoneOn).addChild(redstoneOff);
+
+        rsLabelOn[o] = new ChoiceLabel(mc, this);
+        rsLabelOn[o].addChoices(ProjectorOpcode.getChoices());
+        rsLabelOn[o].setLayoutHint(new PositionalLayout.PositionalHint(8, dy+26, 32, 14));
+        for (ProjectorOpcode operation : ProjectorOpcode.values()) {
+            rsLabelOn[o].setChoiceTooltip(operation.getCode(), operation.getDescription());
+        }
+        sidePanel.addChild(rsLabelOn[o]);
+
+        rsLabelOff[o] = new ChoiceLabel(mc, this);
+        rsLabelOff[o].addChoices(ProjectorOpcode.getChoices());
+        rsLabelOff[o].setLayoutHint(new PositionalLayout.PositionalHint(42, dy+26, 32, 14));
+        for (ProjectorOpcode operation : ProjectorOpcode.values()) {
+            rsLabelOff[o].setChoiceTooltip(operation.getCode(), operation.getDescription());
+        }
+        sidePanel.addChild(rsLabelOff[o]);
+
+        valOn[o] = new TextField(mc, this);
+        valOn[o].setLayoutHint(new PositionalLayout.PositionalHint(8, dy+41, 32, 14));
+        sidePanel.addChild(valOn[o]);
+        valOff[o] = new TextField(mc, this);
+        valOff[o].setLayoutHint(new PositionalLayout.PositionalHint(42, dy+41, 32, 14));
+        sidePanel.addChild(valOff[o]);
+        rsLabelOn[o].addChoiceEvent((parent, newChoice) -> updateRs());
+        rsLabelOff[o].addChoiceEvent((parent, newChoice) -> updateRs());
+        valOn[o].addTextEvent((parent, newText) -> updateRs());
+        valOff[o].addTextEvent((parent, newText) -> updateRs());
+
+        ProjectorOperation op = tileEntity.getOperations()[o];
+        rsLabelOn[o].setChoice(op.getOpcodeOn().getCode());
+        rsLabelOff[o].setChoice(op.getOpcodeOff().getCode());
+        valOn[o].setText(op.getValueOn() == null ? "" : op.getValueOn().toString());
+        valOff[o].setText(op.getValueOff() == null ? "" : op.getValueOff().toString());
+    }
+
+    private void updateRs() {
+        List<Argument> argumentList = new ArrayList<>();
+        for (int i = 0 ; i < 4 ; i++) {
+            argumentList.add(new Argument("opOn" + i, rsLabelOn[i].getCurrentChoice()));
+            argumentList.add(new Argument("opOff" + i, rsLabelOff[i].getCurrentChoice()));
+            String text = valOn[i].getText();
+            if (!text.trim().isEmpty()) {
+                try {
+                    argumentList.add(new Argument("valOn" + i, Double.parseDouble(text)));
+                } catch (NumberFormatException e) {
+                    // Ignore
+                }
+            }
+            text = valOff[i].getText();
+            if (!text.trim().isEmpty()) {
+                try {
+                    argumentList.add(new Argument("valOff" + i, Double.parseDouble(text)));
+                } catch (NumberFormatException e) {
+                    // Ignore
+                }
+            }
+        }
+        sendServerCommand(RFToolsMessages.INSTANCE, ProjectorTileEntity.CMD_RSSETTINGS, argumentList.toArray(new Argument[argumentList.size()]));
     }
 
     @Override
@@ -206,22 +254,6 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
                 new Argument("angle", angleLabel.getRealValue()),
                 new Argument("auto", autoRotate.isPressed())
                 );
-    }
-
-    private void initRedstoneMode() {
-        redstoneMode = new ImageChoiceLabel(mc, this).
-                addChoiceEvent((parent, newChoice) -> changeRedstoneMode()).
-                addChoice(RedstoneMode.REDSTONE_IGNORED.getDescription(), "Redstone mode:\nIgnored", iconGuiElements, 0, 0).
-                addChoice(RedstoneMode.REDSTONE_OFFREQUIRED.getDescription(), "Redstone mode:\nOff to activate", iconGuiElements, 16, 0).
-                addChoice(RedstoneMode.REDSTONE_ONREQUIRED.getDescription(), "Redstone mode:\nOn to activate", iconGuiElements, 32, 0);
-        redstoneMode.setLayoutHint(new PositionalLayout.PositionalHint(5, 180, 16, 16));
-        redstoneMode.setCurrentChoice(tileEntity.getRSMode().ordinal());
-    }
-
-    private void changeRedstoneMode() {
-        tileEntity.setRSMode(RedstoneMode.values()[redstoneMode.getCurrentChoiceIndex()]);
-        sendServerCommand(RFToolsMessages.INSTANCE, ProjectorTileEntity.CMD_MODE,
-                new Argument("rs", RedstoneMode.values()[redstoneMode.getCurrentChoiceIndex()].getDescription()));
     }
 
     @Override
@@ -250,6 +282,12 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
 
         getShapeRenderer().handleMouseWheel();
 
+        for (int i = 0 ; i < 4 ; i++) {
+            ProjectorOperation op = tileEntity.getOperations()[i];
+            valOn[i].setEnabled(op.getOpcodeOn().isNeedsValue());
+            valOff[i].setEnabled(op.getOpcodeOff().isNeedsValue());
+        }
+
         drawWindow();
 
         int currentRF = GenericEnergyStorageTileEntity.getCurrentRF();
@@ -258,7 +296,7 @@ public class GuiProjector extends GenericGuiContainer<ProjectorTileEntity> imple
 
         ItemStack stack = tileEntity.getRenderStack();
         if (ItemStackTools.isValid(stack)) {
-            getShapeRenderer().setShapeID(getShapeID());
+            getShapeRenderer().setShapeID(tileEntity.getShapeID());
             getShapeRenderer().renderShape(this, stack, guiLeft, guiTop, showAxis.isPressed(), showOuter.isPressed(), false);
         }
     }
--- src/main/java/mcjty/rftools/blocks/shaper/ProjectorBlock.java
+++ src/main/java/mcjty/rftools/blocks/shaper/ProjectorBlock.java
@@ -1,9 +1,17 @@
 package mcjty.rftools.blocks.shaper;
 
 import mcjty.lib.container.GenericGuiContainer;
+import mcjty.lib.varia.BlockTools;
 import mcjty.rftools.RFTools;
 import mcjty.rftools.blocks.GenericRFToolsBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -30,6 +38,45 @@ public class ProjectorBlock extends GenericRFToolsBlock<ProjectorTileEntity, Pro
     public boolean needsRedstoneCheck() {
         return true;
     }
+
+    @Override
+    protected void checkRedstone(World world, BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+        TileEntity te = world.getTileEntity(pos);
+        if (state.getBlock() instanceof ProjectorBlock && te instanceof ProjectorTileEntity) {
+            ProjectorTileEntity projector = (ProjectorTileEntity)te;
+            EnumFacing north = BlockTools.reorientHoriz(EnumFacing.NORTH, getMetaFromState(state));
+            EnumFacing south = BlockTools.reorientHoriz(EnumFacing.SOUTH, getMetaFromState(state));
+            EnumFacing west = BlockTools.reorientHoriz(EnumFacing.WEST, getMetaFromState(state));
+            EnumFacing east = BlockTools.reorientHoriz(EnumFacing.EAST, getMetaFromState(state));
+
+            int powered1 = getInputStrength(world, pos, north) > 0 ? 1 : 0;
+            int powered2 = getInputStrength(world, pos, south) > 0 ? 2 : 0;
+            int powered3 = getInputStrength(world, pos, west) > 0 ? 4 : 0;
+            int powered4 = getInputStrength(world, pos, east) > 0 ? 8 : 0;
+            projector.setPowerInput(powered1 + powered2 + powered3 + powered4);
+        }
+    }
+
+    /**
+     * Returns the signal strength at one side of the block
+     */
+    protected int getInputStrength(World world, BlockPos pos, EnumFacing side) {
+        int power = world.getRedstonePower(pos.offset(side), side);
+        if (power == 0) {
+            // Check if there is no redstone wire there. If there is a 'bend' in the redstone wire it is
+            // not detected with world.getRedstonePower().
+            // @todo this is a bit of a hack. Don't know how to do it better right now
+            IBlockState blockState = world.getBlockState(pos.offset(side));
+            Block b = blockState.getBlock();
+            if (b == Blocks.REDSTONE_WIRE) {
+                power = world.isBlockPowered(pos.offset(side)) ? 15 : 0;
+            }
+        }
+
+        return power;
+    }
+
 
     @SideOnly(Side.CLIENT)
     @Override
