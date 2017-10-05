5b0707fde2860a8c869b8dd684256717e54a442e
McJty <mcjty1@gmail.com>
2017-10-05 14:52:29 +0200

    Shape server->client work is now spread over multiple shape id's

--- src/main/java/mcjty/rftools/shapes/ShapeDataManager.java
+++ src/main/java/mcjty/rftools/shapes/ShapeDataManager.java
@@ -7,7 +7,6 @@ import mcjty.rftools.varia.RLE;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -58,14 +57,12 @@ public class ShapeDataManager {
 
 
     private static class WorkUnit {
-        private final ShapeID shapeID;
         private final List<EntityPlayerMP> players = new ArrayList<>();
         private ItemStack stack;
         private int offsetY;
         private IFormula formula;
 
-        public WorkUnit(ShapeID shapeID, ItemStack stack, int offsetY, IFormula formula, EntityPlayerMP player) {
-            this.shapeID = shapeID;
+        public WorkUnit(ItemStack stack, int offsetY, IFormula formula, EntityPlayerMP player) {
             this.stack = stack;
             this.offsetY = offsetY;
             this.formula = formula;
@@ -85,10 +82,6 @@ public class ShapeDataManager {
             return players;
         }
 
-        public ShapeID getShapeID() {
-            return shapeID;
-        }
-
         public ItemStack getStack() {
             return stack;
         }
@@ -102,52 +95,77 @@ public class ShapeDataManager {
         }
     }
 
-    // Server-side
-    private static final ArrayDeque<WorkUnit> workQueue = new ArrayDeque<>();
-    private static final Map<Pair<ShapeID, Integer>, WorkUnit> workingOn = new HashMap<>();
+    private static class WorkQueue {
+        private final ShapeID shapeID;
+        private final ArrayDeque<WorkUnit> workQueue = new ArrayDeque<>();
+        private final Map<Integer, WorkUnit> workingOn = new HashMap<>();
 
-    public static void pushWork(ShapeID shapeID, ItemStack stack, int offsetY, IFormula formula, EntityPlayerMP player) {
-        Pair<ShapeID, Integer> key = Pair.of(shapeID, offsetY);
-        if (workingOn.containsKey(key)) {
-            workingOn.get(key).update(stack, offsetY, formula, player);
-        } else {
-            WorkUnit unit = new WorkUnit(shapeID, stack, offsetY, formula, player);
-            workQueue.addLast(unit);
-            workingOn.put(key, unit);
+        public WorkQueue(ShapeID shapeID) {
+            this.shapeID = shapeID;
         }
     }
 
-    public static int maxworkqueue = 0;
+    // Server-side
+    private static final Map<ShapeID, WorkQueue> workQueues = new HashMap<>();
+
+    public static void pushWork(ShapeID shapeID, ItemStack stack, int offsetY, IFormula formula, EntityPlayerMP player) {
+        WorkQueue queue = workQueues.get(shapeID);
+        if (queue == null) {
+            queue = new WorkQueue(shapeID);
+            workQueues.put(shapeID, queue);
+        }
+        if (queue.workingOn.containsKey(offsetY)) {
+            queue.workingOn.get(offsetY).update(stack, offsetY, formula, player);
+        } else {
+            WorkUnit unit = new WorkUnit(stack, offsetY, formula, player);
+            queue.workQueue.addLast(unit);
+            queue.workingOn.put(offsetY, unit);
+        }
+    }
+
+    private static int maxworkqueue = 0;
 
     public static void handleWork() {
-        int pertick = ScannerConfiguration.planeSurfacePerTick;
-        while (!workQueue.isEmpty()) {
-            if (workQueue.size() > maxworkqueue) {
-                maxworkqueue = workQueue.size();
-                System.out.println("maxworkqueue = " + maxworkqueue);
-            }
-            WorkUnit unit = workQueue.removeFirst();
-            Pair<ShapeID, Integer> key = Pair.of(unit.shapeID, unit.getOffsetY());
-            workingOn.remove(key);
+        Set<ShapeID> toRemove = new HashSet<>();
+        for (Map.Entry<ShapeID, WorkQueue> entry : workQueues.entrySet()) {
+            ShapeID shapeID = entry.getKey();
+            WorkQueue queue = entry.getValue();
 
-            ItemStack card = unit.getStack();
-            boolean solid = ShapeCardItem.isSolid(card);
-            BlockPos dimension = ShapeCardItem.getDimension(card);
+            int pertick = ScannerConfiguration.planeSurfacePerTick;
+            while (!queue.workQueue.isEmpty()) {
+                if (queue.workQueue.size() > maxworkqueue) {
+                    maxworkqueue = queue.workQueue.size();
+                    System.out.println("maxworkqueue = " + maxworkqueue);
+                }
+                WorkUnit unit = queue.workQueue.removeFirst();
+                queue.workingOn.remove(unit.getOffsetY());
 
-            RLE positions = new RLE();
-            StatePalette statePalette = new StatePalette();
-            int cnt = ShapeCardItem.getRenderPositions(card, solid, positions, statePalette, unit.getFormula(), unit.getOffsetY());
+                ItemStack card = unit.getStack();
+                boolean solid = ShapeCardItem.isSolid(card);
+                BlockPos dimension = ShapeCardItem.getDimension(card);
 
-            for (EntityPlayerMP player : unit.getPlayers()) {
-                RFToolsMessages.INSTANCE.sendTo(new PacketReturnShapeData(unit.getShapeID(), positions, statePalette, dimension, cnt, unit.getOffsetY(), ""), player);
-            }
-            if (cnt > 0) {
-                pertick -= dimension.getX() * dimension.getZ();
-                if (pertick <= 0) {
-                    break;
+                RLE positions = new RLE();
+                StatePalette statePalette = new StatePalette();
+                int cnt = ShapeCardItem.getRenderPositions(card, solid, positions, statePalette, unit.getFormula(), unit.getOffsetY());
+
+                for (EntityPlayerMP player : unit.getPlayers()) {
+                    RFToolsMessages.INSTANCE.sendTo(new PacketReturnShapeData(shapeID, positions, statePalette, dimension, cnt, unit.getOffsetY(), ""), player);
+                }
+                if (cnt > 0) {
+                    pertick -= dimension.getX() * dimension.getZ();
+                    if (pertick <= 0) {
+                        break;
+                    }
                 }
             }
+            if (queue.workQueue.isEmpty()) {
+                toRemove.add(shapeID);
+            }
         }
+        for (ShapeID id : toRemove) {
+            workQueues.remove(id);
+        }
+
     }
 
 }
