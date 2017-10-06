d4eb0847a885b5c1a3bd06809eb81d8afe603908
McJty <mcjty1@gmail.com>
2017-10-06 05:19:06 +0200

    Fixed scan sound not stopping if the scan stops

--- src/main/java/mcjty/rftools/blocks/shaper/ProjectorRenderer.java
+++ src/main/java/mcjty/rftools/blocks/shaper/ProjectorRenderer.java
@@ -14,19 +14,23 @@ public class ProjectorRenderer extends TileEntitySpecialRenderer<ProjectorTileEn
     public void renderTileEntityAt(ProjectorTileEntity te, double x, double y, double z, float partialTicks, int destroyStage) {
         super.renderTileEntityAt(te, x, y, z, partialTicks, destroyStage);
 
+        boolean sound = false;
         ItemStack renderStack = te.getRenderStack();
         if (te.isProjecting() && ItemStackTools.isValid(renderStack)) {
             ShapeRenderer renderer = te.getShapeRenderer();
             boolean doSound = renderer.renderShapeInWorld(renderStack, x, y, z, te.getVerticalOffset(), te.getScale(), te.getAngle(),
                     te.isScanline());
-            if (ScannerConfiguration.baseProjectorVolume > 0.0f) {
-                if (doSound && te.isSound()) {
-                    if (!ProjectorSounds.isScanPlaying(te.getPos())) {
-                        ProjectorSounds.playScan(te.getWorld(), te.getPos());
-                    }
-                } else {
-                    ProjectorSounds.stopSound(te.getPos());
+            if (ScannerConfiguration.baseProjectorVolume > 0.0f && doSound && te.isSound()) {
+                sound = true;
+            }
+        }
+        if (ScannerConfiguration.baseProjectorVolume > 0.0f) {
+            if (sound) {
+                if (!ProjectorSounds.isScanPlaying(te.getPos())) {
+                    ProjectorSounds.playScan(te.getWorld(), te.getPos());
                 }
+            } else {
+                ProjectorSounds.stopSound(te.getPos());
             }
         }
     }
