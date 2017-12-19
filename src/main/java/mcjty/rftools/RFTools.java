ecc8ae39775ff28f8f9d3b2dfa9905158af03747
McJty <mcjty1@gmail.com>
2017-12-19 20:35:51 +0100

    7.16

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -43,7 +43,7 @@ import java.util.function.Function;
         version = RFTools.VERSION)
 public class RFTools implements ModBase {
     public static final String MODID = "rftools";
-    public static final String VERSION = "7.15";
+    public static final String VERSION = "7.16";
     public static final String MIN_FORGE_VER = "14.22.0.2464";
     public static final String MIN_MCJTYLIB_VER = "2.5.0";
     public static final String MIN_XNET_VER = "1.5.0";
