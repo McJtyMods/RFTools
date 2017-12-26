ccc7b3c07c62013910218e928b86c91bef26c1ce
McJty <mcjty1@gmail.com>
2017-12-26 05:50:45 +0100

    Bump

--- src/main/java/mcjty/rftools/RFTools.java
+++ src/main/java/mcjty/rftools/RFTools.java
@@ -45,9 +45,9 @@ import java.util.function.Function;
         version = RFTools.VERSION)
 public class RFTools implements ModBase {
     public static final String MODID = "rftools";
-    public static final String VERSION = "7.18";
+    public static final String VERSION = "7.19";
     public static final String MIN_FORGE_VER = "14.22.0.2464";
-    public static final String MIN_MCJTYLIB_VER = "2.5.4";
+    public static final String MIN_MCJTYLIB_VER = "2.5.6";
     public static final String MIN_XNET_VER = "1.5.0";
 
     @SidedProxy(clientSide = "mcjty.rftools.proxy.ClientProxy", serverSide = "mcjty.rftools.proxy.ServerProxy")
