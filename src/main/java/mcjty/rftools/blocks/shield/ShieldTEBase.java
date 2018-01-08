1d40f51ffbcd2949c428287fe2fe69af252c4e94
McJty <mcjty1@gmail.com>
2018-01-08 12:58:47 +0100

    Don't consume power on a shield that is not composed

--- src/main/java/mcjty/rftools/blocks/shield/ShieldTEBase.java
+++ src/main/java/mcjty/rftools/blocks/shield/ShieldTEBase.java
@@ -1,7 +1,6 @@
 package mcjty.rftools.blocks.shield;
 
 import com.mojang.authlib.GameProfile;
-
 import li.cil.oc.api.machine.Arguments;
 import li.cil.oc.api.machine.Callback;
 import li.cil.oc.api.machine.Context;
