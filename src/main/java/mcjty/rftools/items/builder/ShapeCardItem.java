ec07adf855d23fef7411bc3e16862e9250dae70e
McJty <mcjty1@gmail.com>
2018-01-13 19:05:29 +0100

    The 'liquid' card is back. It was accidentally removed

--- src/main/java/mcjty/rftools/items/builder/ShapeCardItem.java
+++ src/main/java/mcjty/rftools/items/builder/ShapeCardItem.java
@@ -881,7 +881,7 @@ public class ShapeCardItem extends GenericRFToolsItem implements INBTPreservingI
     @Override
     public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
         if (this.isInCreativeTab(tab)) {
-            for (int i = 0; i <= 9; i++) {
+            for (int i = 0; i <= 10; i++) {
                 items.add(new ItemStack(this, 1, i));
             }
         }
