7fb4311b85daaba9e1272c39a5c5002a61d86a85
McJty <mcjty1@gmail.com>
2018-06-29 09:21:15 +0200

    Fixed a few baked models that didn't test for null state

--- src/main/java/mcjty/rftools/blocks/shield/CamoBakedModel.java
+++ src/main/java/mcjty/rftools/blocks/shield/CamoBakedModel.java
@@ -39,6 +39,9 @@ public class CamoBakedModel implements IBakedModel {
 
     @Override
     public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand) {
+        if (state == null) {
+            return Collections.emptyList();
+        }
         IExtendedBlockState extendedBlockState = (IExtendedBlockState) state;
         CamoBlockId facadeId = extendedBlockState.getValue(CamoShieldBlock.CAMOID);
         if (facadeId == null) {
