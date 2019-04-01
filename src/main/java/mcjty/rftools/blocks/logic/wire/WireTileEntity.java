c463d65488b45fbe8c3953497083dbc0cfae6f0a
McJty <mcjty1@gmail.com>
2019-04-01 11:03:25 +0200

    Fix a problem with the wireless logic block not correctly sending a block update to the destination block

--- src/main/java/mcjty/rftools/blocks/logic/wire/WireTileEntity.java
+++ src/main/java/mcjty/rftools/blocks/logic/wire/WireTileEntity.java
@@ -6,8 +6,11 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class WireTileEntity extends LogicTileEntity {
+
+    private int loopDetector = 0;
 
     @Override
     public int getRedstoneOutput(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
@@ -15,6 +18,20 @@ public class WireTileEntity extends LogicTileEntity {
             return powerLevel;
         } else {
             return 0;
+        }
+    }
+
+    @Override
+    public void checkRedstone(World world, BlockPos pos) {
+        super.checkRedstone(world, pos);
+        if (loopDetector <= 0) {
+            loopDetector++;
+            IBlockState state = world.getBlockState(pos);
+            BlockPos offsetPos = pos.offset(getFacing(state).getInputSide().getOpposite());
+            if (world.isBlockLoaded(offsetPos)) {
+                world.neighborChanged(offsetPos, state.getBlock(), pos);
+            }
+            loopDetector--;
         }
     }
 }
