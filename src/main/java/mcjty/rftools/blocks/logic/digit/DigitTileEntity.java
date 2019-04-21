977fe24b63ca565db775cae767c9590cd750075e
McJty <mcjty1@gmail.com>
2019-04-21 05:19:46 +0200

    Fixed the digit logic block not always updating visually

--- src/main/java/mcjty/rftools/blocks/logic/digit/DigitTileEntity.java
+++ src/main/java/mcjty/rftools/blocks/logic/digit/DigitTileEntity.java
@@ -4,6 +4,9 @@ package mcjty.rftools.blocks.logic.digit;
 import mcjty.lib.tileentity.LogicTileEntity;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -14,6 +17,15 @@ public class DigitTileEntity extends LogicTileEntity {
 
     public int getPowerLevel() {
         return powerLevel;
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+        int p = getPowerLevel();
+        super.onDataPacket(net, packet);
+        if (p != getPowerLevel()) {
+            world.markBlockRangeForRenderUpdate(pos, pos);
+        }
     }
 
     @Override
@@ -29,7 +41,6 @@ public class DigitTileEntity extends LogicTileEntity {
         setPowerInput(power);
         if (oldPower != power) {
             markDirtyClient();
-//                world.markBlockRangeForRenderUpdate(pos, pos);
         }
     }
 }
